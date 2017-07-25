1f69cdc5bbe511db8d468fdd1d432edf12b5d68b
Petr Široký <psiroky@redhat.com>
2017-07-25 13:33:06 +0200

    Execute downstream PR build automatically
    The downstream PR build will be executed automatically after the
    standard build succeeds (green). It will still be possible to execute
    just the downstream build. The tirgger phrases will now behave like this:
    
      - "jenkins (re)test this" -> executes the standard build and if that one
        succeeds, the downstream build will be triggered automatically
    
      - "jenkins execute full downstream build" -> only the downstream build
        gets triggered, no matter the state of the standard PR build

--- job-dsls/jobs/downstream_pr_jobs.groovy
+++ job-dsls/jobs/downstream_pr_jobs.groovy
@@ -68,6 +68,7 @@ for (repoConfig in REPO_CONFIGS) {
 
     // jobs for master branch don't use the branch in the name
     String jobName = (repoBranch == "master") ? "$repo-downstream-pullrequests" : "$repo-downstream-pullrequests-$repoBranch"
+    String ghBuildContext = "Linux - full downstream"
     job(jobName) {
 
         description("""Created automatically by Jenkins job DSL plugin. Do not edit manually! The changes will be lost next time the job is generated.
@@ -121,7 +122,7 @@ for (repoConfig in REPO_CONFIGS) {
                 whiteListTargetBranches([repoBranch])
                 extensions {
                     commitStatus {
-                        context('Linux - full downstream')
+                        context(ghBuildContext)
                         addTestResults(true)
                     }
 
@@ -139,6 +140,10 @@ for (repoConfig in REPO_CONFIGS) {
             }
             timestamps()
             colorizeOutput()
+            downstreamCommitStatus {
+                context(ghBuildContext)
+                addTestResults(true)
+            }
         }
 
         steps {
