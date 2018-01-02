bdf45e0196152df57db0cd432813d0cd2d609488
Petr Široký <siroky.petr@gmail.com>
2018-01-02 14:26:58 +0100

    psiroky is no longer primary owner of the jobs

--- job-dsls/jobs/downstream_pr_jobs.groovy
+++ job-dsls/jobs/downstream_pr_jobs.groovy
@@ -109,8 +109,8 @@ for (repoConfig in REPO_CONFIGS) {
 
         properties {
             ownership {
-                primaryOwnerId("psiroky")
-                coOwnerIds("mbiarnes")
+                primaryOwnerId("mbiarnes")
+                coOwnerIds("pszubiak", "anstephe")
             }
         }
 
