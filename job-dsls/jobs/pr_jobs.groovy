ec558d5cc2051e88abf936500fa4d12c63d8ec5d
Piotr Szubiakowski <pszubiak@redhat.com>
2018-06-04 10:59:15 +0200

    Set cron trigger frequency for PR and blessed jobs to 20 minutes. (#225)
    Signed-off-by: Piotr Szubiakowski <pszubiak@redhat.com>

--- job-dsls/jobs/pr_jobs.groovy
+++ job-dsls/jobs/pr_jobs.groovy
@@ -161,7 +161,7 @@ for (repoConfig in REPO_CONFIGS) {
             githubPullRequest {
                 orgWhitelist(["appformer", "kiegroup"])
                 allowMembersOfWhitelistedOrgsAsAdmin()
-                cron("H/10 * * * *")
+                cron("H/20 * * * *")
                 whiteListTargetBranches([repoBranch])
                 extensions {
                     commitStatus {
