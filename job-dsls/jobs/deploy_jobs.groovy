a176875dfa821c50223904cc777fda791d80e704
Piotr Szubiakowski <pszubiak@redhat.com>
2018-06-05 14:54:58 +0200

    Set cron trigger frequency for PR and blessed jobs to 30 minutes. (#231)
    Signed-off-by: Piotr Szubiakowski <pszubiak@redhat.com>

--- job-dsls/jobs/deploy_jobs.groovy
+++ job-dsls/jobs/deploy_jobs.groovy
@@ -215,7 +215,7 @@ for (repoConfig in REPO_CONFIGS) {
         label(get("label"))
 
         triggers {
-            scm('H/20 * * * *')
+            scm('H/30 * * * *')
         }
 
         wrappers {
