64b4028dd2f45a777de23c40e2d64f4c0aa105c3
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2018-07-26 12:58:31 +0200

    disabled temporary zanata jobs

--- job-dsls/jobs/zanataPush.groovy
+++ job-dsls/jobs/zanataPush.groovy
@@ -1,7 +1,7 @@
 //Define Variables
 
 def kieMainBranch="master"
-def zanataVersion="7.8.0"
+def zanataVersion="7.9.0"
 def settingsXml="org.jenkinsci.plugins.configfiles.custom.CustomConfig1457025283676"
 
 def organization="kiegroup"
@@ -17,6 +17,9 @@ sh \$WORKSPACE/scripts/droolsjbpm-build-bootstrap/script/zanata/zanata-pushModul
 """
 
 job("zanataPushModules-${zanataVersion}") {
+
+    // temporary disabled until all jobs are migrated to RHBA Jenkins
+    disabled()
 
     description("This job: <br> pushes the i18n files to https://vendors.zanata.redhat.com<br>IMPORTANT: Created automatically by Jenkins job DSL plugin. Do not edit manually! The changes will get lost next time the job is generated.")
 
