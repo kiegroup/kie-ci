61ab7d01c96164702596720770b0a834a14c6a02
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2018-09-03 09:45:03 +0200

    updated for 7.11.x branch

--- job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
+++ job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
@@ -20,7 +20,7 @@ class Constants {
     static final String MAVEN_VERSION = "3.5.2"
     static final String UPSTREAM_BUILD_MAVEN_VERSION = "3.5.2"
     static final String JDK_VERSION = "kie-jdk1.8"
-    static final String BRANCH = "master"
+    static final String BRANCH = "7.11.x"
     static final String GITHUB_ORG_UNIT = "kiegroup"
     static final String GITHUB_AUTH_TOKEN = "3e0e2db0-bf45-4c5b-b97f-9542aca56f44"
     static final String PULL_REQUEST_FOLDER = "pullrequest"
