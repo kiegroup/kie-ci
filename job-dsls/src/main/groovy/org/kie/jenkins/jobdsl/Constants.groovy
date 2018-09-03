2ea4f609d2ad7b355559f886cce09f30b30f0bac
Piotr Szubiakowski <pszubiak@redhat.com>
2018-09-03 10:39:08 +0200

    Set kiereleaseuser3 token for 7.11.x branch.
    Signed-off-by: Piotr Szubiakowski <pszubiak@redhat.com>

--- job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
+++ job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
@@ -22,7 +22,7 @@ class Constants {
     static final String JDK_VERSION = "kie-jdk1.8"
     static final String BRANCH = "7.11.x"
     static final String GITHUB_ORG_UNIT = "kiegroup"
-    static final String GITHUB_AUTH_TOKEN = "3e0e2db0-bf45-4c5b-b97f-9542aca56f44"
+    static final String GITHUB_AUTH_TOKEN = "e9d5d102-3b5b-48cb-af5d-7a91c1dd7be5"
     static final String PULL_REQUEST_FOLDER = "pullrequest"
     static final String DEPLOY_FOLDER = "deployedRep"
 }
