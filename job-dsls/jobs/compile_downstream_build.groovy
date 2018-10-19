1948bac34f4e0922b7cddf992368f5b037f9cd87
Piotr Szubiakowski <pszubiak@redhat.com>
2018-10-19 11:56:43 +0200

    Change github message for compile downstream build. (#359)
    Signed-off-by: Piotr Szubiakowski <pszubiak@redhat.com>

--- job-dsls/jobs/compile_downstream_build.groovy
+++ job-dsls/jobs/compile_downstream_build.groovy
@@ -119,7 +119,7 @@ for (repoConfig in REPO_CONFIGS) {
                 whiteListTargetBranches([repoBranch])
                 extensions {
                     commitStatus {
-                        context('Linux - full downstream')
+                        context('Linux - compile downstream')
                         addTestResults(true)
                     }
 
