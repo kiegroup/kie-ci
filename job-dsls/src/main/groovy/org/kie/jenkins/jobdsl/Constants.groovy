3449512b0e8deba0e6eaa9a6ed894572de8af34f
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2018-12-03 09:21:52 +0100

    upgraded to latest Zanata version

--- job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
+++ job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
@@ -25,12 +25,12 @@ class Constants {
     static final String GITHUB_AUTH_TOKEN = "3e0e2db0-bf45-4c5b-b97f-9542aca56f44"
     static final String PULL_REQUEST_FOLDER = "pullrequest"
     static final String DEPLOY_FOLDER = "deployedRep"
-    static final String ZANATA_VERSION = "7.15.0"
+    static final String ZANATA_VERSION = "7.16.0"
     static final String ERRAI_BRANCH = "master"
     static final String ERRAI_SNAPSHOT = "4.4.2-SNAPSHOT"
     static final String ERRAI_PREFIX = "4.4.2"
-    static final String KIE_PREFIX = "7.15.0"
-    static final String KIE_PROD_BRANCH_PREFIX = "7.15.x"
+    static final String KIE_PREFIX = "7.16.0"
+    static final String KIE_PROD_BRANCH_PREFIX = "7.16.x"
     static final String UBERFIRE_PREFIX = "2.12.0"
 
 }
