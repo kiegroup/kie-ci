3ebba5883330894b68712513023978accb5437c0
kieciuser <kieciuser@gmail.com>
2019-01-31 12:01:06 +0100

    upgraded constants

--- job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
+++ job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
@@ -25,12 +25,12 @@ class Constants {
     static final String GITHUB_AUTH_TOKEN = "3e0e2db0-bf45-4c5b-b97f-9542aca56f44"
     static final String PULL_REQUEST_FOLDER = "pullrequest"
     static final String DEPLOY_FOLDER = "deployedRep"
-    static final String ZANATA_VERSION = "7.17.0"
+    static final String ZANATA_VERSION = "7.18.0"
     static final String ERRAI_BRANCH = "master"
     static final String ERRAI_SNAPSHOT = "4.5.0-SNAPSHOT"
     static final String ERRAI_PREFIX = "4.5.0"
-    static final String KIE_PREFIX = "7.17.0"
-    static final String KIE_PROD_BRANCH_PREFIX = "7.17.x"
-    static final String UBERFIRE_PREFIX = "2.14.0"
+    static final String KIE_PREFIX = "7.18.0"
+    static final String KIE_PROD_BRANCH_PREFIX = "7.18.x"
+    static final String UBERFIRE_PREFIX = "2.15.0"
 
 }
