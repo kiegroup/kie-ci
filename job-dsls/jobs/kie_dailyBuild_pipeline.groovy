1a86a6209fea39e4b3aeacb3292a0385e69205a3
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2019-02-12 11:07:24 +0100

    removed script that removes all created artifacts (#409)

--- job-dsls/jobs/kie_dailyBuild_pipeline.groovy
+++ job-dsls/jobs/kie_dailyBuild_pipeline.groovy
@@ -467,18 +467,10 @@ cd ..
 ./droolsjbpm-build-bootstrap/script/git-all.sh add .
 ./droolsjbpm-build-bootstrap/script/git-all.sh commit -m "upgraded version"
 
-cat > "$WORKSPACE/clean-up.sh" << EOT
-cd \\$1
-# Add test reports to the index to prevent their removal in the following step
-git add --force **target/*-reports/TEST-*.xml
-# Remove all build artifacts to save space
-git clean -ffdx
-EOT
-
 # do a full build
 ./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean install -Dfull -Drelease -Dproductized -s $SETTINGS_XML_FILE\\
  -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dmaven.test.redirectTestOutputToFile=true -Dmaven.test.failure.ignore=true\\
- -Dgwt.memory.settings="-Xmx10g" --clean-up-script="$WORKSPACE/clean-up.sh"
+ -Dgwt.memory.settings="-Xmx10g"
  
 # creates a tarball with all repositories and saves it on Jenkins master
 tar czf prodBranches.tgz *
@@ -538,7 +530,7 @@ job("${folderPath}/prod-kieAllBuild-${kieMainBranch}") {
         archiveArtifacts{
             onlyIfSuccessful(false)
             allowEmpty(true)
-            pattern("prodBranches.tgz,**/target/business-central*wildfly*.war,**/target/business-central*eap*.war")
+            pattern("prodBranches.tgz, **/target/business-central-*-eap*.war, **/target/business-central-*-wildfly*.war")
         }
         mailer('mbiarnes@redhat.com pszubiak@redhat.com anstephe@redhat.com', false, false)
         wsCleanup()
