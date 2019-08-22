bdc55d3c77deab6211753db3d96231335ab59ed4
Karel Suta <ksuta@redhat.com>
2019-08-22 10:10:26 +0200

    Enable SonarCloud scan for droolsjbpm-integration PRs

--- job-dsls/jobs/pr_jobs.groovy
+++ job-dsls/jobs/pr_jobs.groovy
@@ -120,7 +120,7 @@ def final REPO_CONFIGS = [
 
 ]
 
-def final SONARCLOUD_ENABLED_REPOSITORIES = ["optaplanner", "drools", "appformer", "jbpm", " drools-wb", "kie-soup"]
+def final SONARCLOUD_ENABLED_REPOSITORIES = ["optaplanner", "drools", "appformer", "jbpm", " drools-wb", "kie-soup", "droolsjbpm-integration"]
 
 for (repoConfig in REPO_CONFIGS) {
     Closure<Object> get = { String key -> repoConfig.value[key] ?: DEFAULTS[key] }
