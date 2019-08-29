ac304fa247d2bc1c60d48cef391790bd4f7fd068
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-08-29 13:37:02 +0200

    changed groovy scripts to build

--- job-dsls/jobs/seed_job.groovy
+++ job-dsls/jobs/seed_job.groovy
@@ -1,8 +1,3 @@
-/**
- * Please look at the *.groovy jobs that have to be in this branch.
- * Remove not needed ones or add new and needed ones.
- */
-
 import org.kie.jenkins.jobdsl.Constants
 
 // definition of parameters
@@ -62,18 +57,7 @@ job("${folderPath}/a-seed-job-${kieMainBranch}") {
         shell(seedJob)
 
         jobDsl {
-            targets("job-dsls/jobs/**/bxms_patch_tools_pr.groovy\n" +
-                    "job-dsls/jobs/**/pr_jobs.groovy\n" +
-                    "job-dsls/jobs/**/downstream_pr_jobs.groovy\n" +
-                    "job-dsls/jobs/**/deploy_jobs.groovy\n" +
-                    "job-dsls/jobs/**/compile_downstream_build.groovy\n" +
-                    "job-dsls/jobs/**/sonarcloud_daily.groovy\n" +
-                    "job-dsls/jobs/**/springboot_pr_job.groovy\n" +
-                    "job-dsls/jobs/**/kogito.groovy\n" +
-                    "job-dsls/jobs/**/kie*.groovy\n" +
-                    "job-dsls/jobs/**/turtleTests.groovy\n" +
-                    "job-dsls/jobs/**/zanata*.groovy")
-
+            targets("job-dsls/jobs/**/*.groovy")
             useScriptText(false)
             sandbox(false)
             ignoreExisting(false)
