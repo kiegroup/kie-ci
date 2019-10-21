c8b9f84fa47b7f3a142ee092c538275762d37479
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-10-21 12:27:27 +0200

    changed email recipients of additional test

--- job-dsls/jobs/DailyBuild_pipeline.groovy
+++ job-dsls/jobs/DailyBuild_pipeline.groovy
@@ -96,7 +96,7 @@ pipeline {
             steps {
                 emailext body: 'daily build #${BUILD_NUMBER} of ${baseBranch} was:' + "${currentBuild.currentResult}" +  '\\n' +
                     'Please look here: ${BUILD_URL} \\n' +
-                    '${BUILD_LOG, maxLines=750}', subject: 'daily build of  #${BUILD_NUMBER} of ${baseBranch}: ' + "${currentBuild.currentResult}", to: 'bsig@redhat.com'
+                    '${BUILD_LOG, maxLines=750}', subject: 'daily build #${BUILD_NUMBER} of ${baseBranch}: ' + "${currentBuild.currentResult}", to: 'bsig@redhat.com'
             }    
         }        
         stage('Unpack zip of artifacts to QA Nexus') {
@@ -256,7 +256,7 @@ matrixJob("${folderPath}/DailyBuild-jbpmTestCoverageMatrix-${baseBranch}") {
 
     publishers {
         archiveJunit("**/target/*-reports/TEST-*.xml")
-        mailer('mbiarnes@redhat.com', false, false)
+        mailer('bsig@redhat.com', false, false)
         wsCleanup()
     }
 
@@ -330,7 +330,7 @@ matrixJob("${folderPath}/DailyBuild-jbpmTestContainerMatrix-${baseBranch}") {
 
     publishers {
         archiveJunit("**/target/*-reports/TEST-*.xml")
-        mailer('mbiarnes@redhat.com', false, false)
+        mailer('bsig@redhat.com', false, false)
         wsCleanup()
     }
 
@@ -434,7 +434,7 @@ matrixJob("${folderPath}/DailyBuild-kieWbTestsMatrix-${baseBranch}") {
 
     publishers {
         archiveJunit("**/target/*-reports/TEST-*.xml, **/target/screenshots/*")
-        mailer('mbiarnes@redhat.com', false, false)
+        mailer('bsigs@redhat.com', false, false)
         wsCleanup()
     }
 
@@ -515,7 +515,7 @@ matrixJob("${folderPath}/DailyBuild-kieServerMatrix-${baseBranch}") {
 
     publishers {
         archiveJunit("**/target/*-reports/TEST-*.xml")
-        mailer('mbiarnes@redhat.com', false, false)
+        mailer('bsig@redhat.com', false, false)
         wsCleanup()
     }
 
