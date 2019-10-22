4af9ea8444be2e65bcb03428e4559459ebe04b62
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-10-22 10:52:07 +0200

    new pipelines for daily builds, product tags and community releases

---
+++ job-dsls/jobs/DailyBuild_prod_pipeline.groovy
@@ -0,0 +1,133 @@
+import org.kie.jenkins.jobdsl.Constants
+
+def baseBranch=Constants.BRANCH
+def organization=Constants.GITHUB_ORG_UNIT
+def kieVersion=Constants.KIE_PREFIX
+def m2Dir="\$HOME/.m2/repository"
+
+
+// creation of folder
+folder("DailyBuild-prod")
+
+def folderPath="DailyBuild-prod"
+
+def dailyProdBuild='''
+pipeline {
+    agent {
+        label 'kie-linux&&kie-rhel7&&kie-mem24g'
+    }
+    tools {
+        maven 'kie-maven-3.5.2'
+        jdk 'kie-jdk1.8'
+    }
+    stages {
+        stage('CleanWorkspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Calculate versions') {
+            steps {
+                script {
+                    dataProd = new Date().format('yyyyMMdd')
+                    kieVersion = "${kieVersion}.${dataProd}-prod"
+                    dockerAbsPath = "KIE/${baseBranch}/Docker"
+
+                    echo "dataProd: ${dataProd}"
+                    echo "kieVersion: ${kieVersion}"
+                    echo "baseBranch: ${baseBranch}"
+                    echo "organization: ${organization}"
+                }
+            }
+        }
+        stage ('Checkout droolsjbpm-build-boostrap') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '$baseBranch']], browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/$organization/droolsjbpm-build-bootstrap'], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'droolsjbpm-build-bootstrap']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/$organization/droolsjbpm-build-bootstrap.git']]])
+                dir("${WORKSPACE}" + '/droolsjbpm-build-bootstrap') {
+                    sh 'pwd \\n' +
+                       'git branch \\n' +
+                       'git checkout -b $baseBranch\'
+                } 
+            }
+        }
+        stage('Clone all other reps') {
+            steps {
+                sh "sh droolsjbpm-build-bootstrap/script/release/01_cloneBranches.sh $baseBranch"
+            }
+        }
+        stage('Update versions') {
+            steps {
+                sh "echo 'kieVersion: $kieVersion'"
+                sh "sh droolsjbpm-build-bootstrap/script/release/03_upgradeVersions.sh $kieVersion"
+            }
+        }
+        stage('Create clean up script') {
+            steps {
+                sh 'cat > "$WORKSPACE/clean-up.sh" << EOT \\n' +
+                        'cd \\\\$1 \\n' +
+                        '# Add test reports to the index to prevent their removal in the following step \\n' +
+                        'git add --force **target/*-reports/TEST-*.xml \\n' +
+                        'git clean -ffdx \\n' +
+                        'EOT'
+            }
+        }
+        stage('Clean install'){
+            steps {
+                configFileProvider([configFile(fileId: '771ff52a-a8b4-40e6-9b22-d54c7314aa1e', targetLocation: 'jenkins-settings.xml', variable: 'SETTINGS_XML_FILE')]) {
+                    sh "sh droolsjbpm-build-bootstrap/script/release/05d_dailyBuildProdInstall.sh $SETTINGS_XML_FILE"
+                }
+            }
+        }
+        stage('Publish JUnit test results reports') {
+            steps {
+              junit '**/target/*-reports/TEST-*.xml'    
+            }
+        }        
+        stage ('Send mail') {
+            steps {
+                emailext body: 'prod daily build #${BUILD_NUMBER} of ${baseBranch} was:' + "${currentBuild.currentResult}" +  '\\n' +
+                    'Please look here: ${BUILD_URL} \\n' +
+                    '${BUILD_LOG, maxLines=750}', subject: 'prod daily build #${BUILD_NUMBER} of ${baseBranch}: ' + "${currentBuild.currentResult}", to: 'bsig@redhat.com'
+            }    
+        }        
+        stage('Delete workspace when build is done') {
+            steps {
+                cleanWs()
+            }
+        }                
+    }
+}
+'''
+
+
+pipelineJob("${folderPath}/DailyBuild-prod-pipeline-${baseBranch}") {
+
+    description('this is a prod-pipeline job for the daily build of all reps')
+
+
+    parameters{
+        stringParam("kieVersion", "${kieVersion}", "Version of kie. This will be usually set automatically by the parent pipeline job. ")
+        stringParam("baseBranch", "${baseBranch}", "kie branch. This will be usually set automatically by the parent pipeline job. ")
+        stringParam("organization", "${organization}", "Name of organization. This will be usually set automatically by the parent pipeline job. ")
+    }
+
+    logRotator {
+        numToKeep(10)
+        daysToKeep(10)
+    }
+
+    definition {
+        cps {
+            script("${dailyProdBuild}")
+            sandbox()
+        }
+    }
+
+    publishers {
+        buildDescription ("KIE version ([^\\s]*)")
+        mailer('mbiarnes@redhat.com', false, false)
+    }
+
+}
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
--- job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
+++ job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/Constants.groovy
@@ -27,6 +27,5 @@ class Constants {
     static final String ZANATA_VERSION = "7.26.0"
     static final String KIE_PREFIX = "7.26.1"
     static final String KIE_PROD_BRANCH_PREFIX = "7.26.x"
-    static final String NUMBER_OF_KIE_USERS = "10"
-    static final String SONARCLOUD_FOLDER = "sonarcloud"
+    static final String REPORT_BRANCH = "7.x"
 }
