fb573fd4ca4be8a97f55723594f5ae56705ea359
Enrique Mingorance Cano <emingora@redhat.com>
2019-11-08 12:22:41 +0100

    BXMSPROD-512 ~/.local/bin folder added to PATH

--- job-dsls/jobs/execute_pig.groovy
+++ job-dsls/jobs/execute_pig.groovy
@@ -41,7 +41,7 @@ job(jobName) {
             }
             extensions {
                 cloneOptions {
-                    reference("/home/jenkins/git-repos/build-configurations.git")
+                    reference("git-repos/build-configurations.git")
                 }
             }
         }
@@ -59,6 +59,6 @@ job(jobName) {
     jdk("kie-jdk1.8")
 
     steps {
-        shell("java -DskipBranchCheck -jar /opt/tools/pig/product-files-generator.jar -c /home/jenkins/git-repos/build-configurations.git/\${buildConfiguration} -v scmRevision=\${scmRevision} \${additionalParameters}")
+        shell("java -DskipBranchCheck -jar /opt/tools/pig/product-files-generator.jar -c $WORKSPACE/git-repos/build-configurations.git/\${buildConfiguration} -v scmRevision=\${scmRevision} \${additionalParameters}")
     }
 }
