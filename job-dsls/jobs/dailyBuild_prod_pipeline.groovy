e6d337bbba470dedaa946fe07bd073a4491d6fce
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2020-01-14 10:14:47 +0100

    small fixes and new mail address
    (cherry picked from commit 302779352d3b8779e541cdda34affd67dad29c5c)

--- job-dsls/jobs/dailyBuild_prod_pipeline.groovy
+++ job-dsls/jobs/dailyBuild_prod_pipeline.groovy
@@ -94,7 +94,7 @@ pipeline {
             emailext body: 'prod daily build #${BUILD_NUMBER} (${baseBranch} branch) was: ' + "${currentBuild.currentResult}" +  '\\n' +
                 'Please look here: ${BUILD_URL} \\n' +
                 ' \\n' +                 
-                '${BUILD_LOG, maxLines=750}', subject: 'prod-daily-build-${baseBranch} #${BUILD_NUMBER}: ' + "${currentBuild.currentResult}", to: 'bsig@redhat.com'
+                '${BUILD_LOG, maxLines=750}', subject: 'prod-daily-build-${baseBranch} #${BUILD_NUMBER}: ' + "${currentBuild.currentResult}", to: 'kie-jenkins-builds@redhat.com'
         }
         unstable{
             emailext body: 'prod daily build #${BUILD_NUMBER} of ${baseBranch} was:' + "${currentBuild.currentResult}" +  '\\n' +
@@ -102,11 +102,11 @@ pipeline {
                 ' \\n' +                
                 'Failed tests: ${BUILD_URL}/testReport \\n' +
                 ' \\n' +                 
-                '${BUILD_LOG, maxLines=750}', subject: 'prod-daily-build-${baseBranch} #${BUILD_NUMBER}: ' + "${currentBuild.currentResult}", to: 'bsig@redhat.com'    
+                '${BUILD_LOG, maxLines=750}', subject: 'prod-daily-build-${baseBranch} #${BUILD_NUMBER}: ' + "${currentBuild.currentResult}", to: 'kie-jenkins-builds@redhat.com'    
         }
         success{
             emailext body: 'prod daily build #${BUILD_NUMBER} of ${baseBranch} was:' + "${currentBuild.currentResult}" +  '\\n' +
-                'Please look here: ${BUILD_URL},subject: 'prod-daily-build-${baseBranch} #${BUILD_NUMBER}: ' + "${currentBuild.currentResult}" to: 'mbiarnes@redhat.com, mnovotny@redhat.com'            
+                'Please look here: ${BUILD_URL}', subject: 'prod-daily-build-${baseBranch} #${BUILD_NUMBER}: ' + "${currentBuild.currentResult}", to: 'mbiarnes@redhat.com, mnovotny@redhat.com'            
         }        
     }    
 }
