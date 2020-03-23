9f587af193fe0b7ffba6aa8fa2c8ee57876ea17a
akoufoudakis <akoufoudakis@gmail.com>
2020-03-23 16:35:09 +0100

    Added build params

--- job-dsls/jobs/srcclr_scan_seed.groovy
+++ job-dsls/jobs/srcclr_scan_seed.groovy
@@ -5,6 +5,18 @@ String jobDescription = "Job responsible for SourceClear verification"
 
 def jobDefinition = job("srcclr-scan") {
 
+
+    String debug = config.debug?.toBoolean() ? '-d' : ''
+    String scanType = config.scanType?.trim()
+    String product = config.product?.trim() ?: ''
+    String version = config.version?.trim() ?: ''
+    String packageName = config.packageName ? "--package=${config.packageName.trim()}" : ''
+    String url = config.url?.trim()
+    String extra = config.extra ?: ''
+    String recurse = config.recurse?.trim() ? '--recursive' : ''
+    String scmVersionParam = ''
+
+
     label("kie-rhel7")
 
     wrappers {
