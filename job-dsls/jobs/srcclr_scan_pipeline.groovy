a98315a2235ff2302ce3f67361bddd7fcf6d361a
akoufoudakis <akoufoudakis@gmail.com>
2020-04-08 19:21:25 +0200

    Added more jobs for sourcclear scanning

---
+++ job-dsls/jobs/srcclr_scan_pipeline.groovy
@@ -0,0 +1,65 @@
+import org.kie.jenkins.jobdsl.Constants
+
+def prefix = Constants.GITHUB_REPO_PREFIX
+def repoListFileId = Constants.REPO_LIST_FILE_ID
+def srcclrFolder = Constants.SRCCLR_FOLDER_PATH
+
+def pipelineScript =
+        '''
+@Library('jenkins-pipeline-shared-libraries')_
+
+def repoList = []
+node('kie-rhel7') {
+    stage('Read repo file') {
+        configFileProvider([configFile(fileId: "${repoListFileId}", variable:'repoListFile')]) {
+            def repoFile = readFile "$repoListFile"
+            repoList = repoFile.readLines()
+        }
+    }
+}
+
+def branches = [:]
+
+for (repo in repoList) {
+    def branchName = 'Source Clear ' + "${repo}"
+    println 'Repo before node ' + "${repo}"
+    def repoName = "${repo}"
+    branches[branchName] = {
+        node('kie-rhel7') {
+            stage(branchName) {
+                def url = "${prefix}" + "${repoName}"
+                def jobName = "${srcclrFolder}" + '/srcclr-scan-' + "${repoName}"
+                build job: "${jobName}", propagate: false, parameters: [
+                            [$class: 'StringParameterValue', name: 'SCAN_TYPE', value: 'scm'],
+                            [$class: 'StringParameterValue', name: 'URL', value: "${url}"],
+                            [$class: 'StringParameterValue', name: 'VERSION', value: "${kieVersion}"],
+                            [$class: 'StringParameterValue', name: 'NAME', value: "${repoName}"],
+                            [$class: 'StringParameterValue', name: 'PROCESSOR_TYPE', value: 'cve'],
+                            [$class: 'StringParameterValue', name: 'THRESHOLD', value: '1']
+                ]
+
+            }
+        }
+    }
+}
+
+parallel branches
+
+        '''
+
+pipelineJob("parallel source clear scanning") {
+
+    description("This is a pipeline, which runs source clear scanning jobs")
+
+    parameters {
+        stringParam('kieVersion')
+    }
+
+    definition {
+        cps {
+            script("${pipelineScript}")
+            sandbox()
+        }
+    }
+
+}
--- job-dsls/jobs/srcclr_scan_seed.groovy
+++ job-dsls/jobs/srcclr_scan_seed.groovy
@@ -1,29 +1,38 @@
 import org.kie.jenkins.jobdsl.Constants
 import org.kie.jenkins.jobdsl.templates.BasicJob
 
-String jobDescription = "Job responsible for SourceClear verification"
-
-def jobDefinition = job("srcclr-scan") {
-
-
-    parameters {
-        choiceParam('SCAN_TYPE', ['scm', 'binary'])
-        stringParam('URL','')
-        stringParam('VERSION', '')
-        stringParam('PACKAGE','')
-        stringParam('NAME', '')
-        stringParam('MVNPARAMS', '')
-        choiceParam('PROCESSOR_TYPE', ['cve', 'cvss'])
-        booleanParam('RECURSIVE', false)
-        booleanParam('DEBUGGING', false)
-        booleanParam('TRACING', false)
-        stringParam('SCMVERSION', '')
-        stringParam('THRESHOLD', '1','Threshold from 1 to 10 for cvss processor')
-    }
+def repoList = [
+        'lienzo-core',
+        'drools'
+]
 
 
-    environmentVariables{
-        groovy('''
+for (repo in repoList) {
+
+    def jobName = 'srcclr/srcclr-scan-' + "${repo}"
+
+    job(jobName) {
+
+        String jobDescription = 'Job responsible for SourceClear verification of' + "${repo}"
+
+        parameters {
+            choiceParam('SCAN_TYPE', ['scm', 'binary'])
+            stringParam('URL','')
+            stringParam('VERSION', '')
+            stringParam('PACKAGE','')
+            stringParam('NAME', '')
+            stringParam('MVNPARAMS', '')
+            choiceParam('PROCESSOR_TYPE', ['cve', 'cvss'])
+            booleanParam('RECURSIVE', false)
+            booleanParam('DEBUGGING', false)
+            booleanParam('TRACING', false)
+            stringParam('SCMVERSION', '')
+            stringParam('THRESHOLD', '1','Threshold from 1 to 10 for cvss processor')
+        }
+
+
+        environmentVariables{
+            groovy('''
           def map = [:]
                
           map.put("RECURSE", Boolean.valueOf("${RECURSIVE}") ? "--recursive" : "")
@@ -32,68 +41,52 @@ def jobDefinition = job("srcclr-scan") {
           
           map.put("TRACE", Boolean.valueOf("${TRACING}") ? "--trace" : "")
           
-          if ( "${MVNPARAMS}" != "" )
-          {
+          map.put("MVNPARAMETER", "${MVNPARAMS}" !="" ? "--maven-param=\\"${MVNPARAMS}\\":"")
           
-            map.put("MVNPARAMETER","--maven-param=\\"${MVNPARAMS}\\"")
-            
-          } else {
-          
-            map.put("MVNPARAMETER", "")
-            
-          }
-          
-          if ( "${SCAN_TYPE}" == "scm" ) {
-          
-            map.put("SCMVERSIONPARAM"," --ref=${SCMVERSION}")
-            
-          } else {
-            map.put("SCMVERSIONPARAM","")
-          }
+          map.put("SCMVERSIONPARAM", "${SCAN_TYPE}" == "scm" ? " --ref=${SCMVERSION}":"")
           
           return map
-          
-          return []
            
         ''')
-    }
-
-
-    label("kie-rhel7")
-
-    wrappers {
-        credentialsBinding {
-            string("SRCCLR_API_TOKEN", "SRCCLR_API_TOKEN")
         }
-    }
 
-    scm {
-        git {
-            remote {
-                name('origin')
-                url('https://github.com/project-ncl/sourceclear-invoker')
+
+        label("kie-rhel7")
+
+        wrappers {
+            credentialsBinding {
+                string("SRCCLR_API_TOKEN", "SRCCLR_API_TOKEN")
             }
-            branch('master')
         }
-    }
-    steps {
-        maven {
-            mavenInstallation("kie-maven-3.5.4")
-            goals("-Pjenkins test -Dmaven.buildNumber.skip=true -DargLine='' -Dsourceclear=\"\${DEBUG} \${TRACE} --processor=\${PROCESSOR_TYPE} --product-version=\${VERSION} --package=\${PACKAGE} --product=\"\${NAME}\" --threshold=\${THRESHOLD} \${SCAN_TYPE} --url=\${URL} \${MVNPARAMETER} \${SCMVERSIONPARAM} \${RECURSE}\"")
+
+        scm {
+            git {
+                remote {
+                    name('origin')
+                    url('https://github.com/project-ncl/sourceclear-invoker')
+                }
+                branch('master')
+            }
+        }
+        steps {
+            maven {
+                mavenInstallation("kie-maven-3.5.4")
+                goals("-Pjenkins test -Dmaven.buildNumber.skip=true -DargLine='' -Dsourceclear=\"\${DEBUG} \${TRACE} --processor=\${PROCESSOR_TYPE} --product-version=\${VERSION} --package=\${PACKAGE} --product=\"\${NAME}\" --threshold=\${THRESHOLD} \${SCAN_TYPE} --url=\${URL} \${MVNPARAMETER} \${SCMVERSIONPARAM} \${RECURSE}\"")
+            }
+        }
+
+        publishers {
+            archiveJunit("**/target/*-reports/*.xml") {
+                retainLongStdout()
+                healthScaleFactor(1.0)
+                allowEmptyResults()
+            }
+            archiveArtifacts {
+                pattern("**/target/screenshots/*")
+                allowEmpty()
+            }
         }
     }
 
-    publishers {
-        archiveJunit("**/target/*-reports/*.xml") {
-            retainLongStdout()
-            healthScaleFactor(1.0)
-            allowEmptyResults()
-        }
-        archiveArtifacts {
-            pattern("**/target/screenshots/*")
-            allowEmpty()
-        }
-    }
+    BasicJob.addCommonConfiguration(jobDefinition, jobDescription)
 }
-
-BasicJob.addCommonConfiguration(jobDefinition, jobDescription)
