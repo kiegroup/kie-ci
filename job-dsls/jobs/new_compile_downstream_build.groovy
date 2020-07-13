031336aab2be656d4ee0b9d76f88eefe68ca05d1
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2020-07-13 13:12:05 +0200

    droolsjbpm-tools added as downstream repo in FDBs and CDBs (#781)

--- job-dsls/jobs/new_compile_downstream_build.groovy
+++ job-dsls/jobs/new_compile_downstream_build.groovy
@@ -28,7 +28,7 @@ def final REPO_CONFIGS = [
         "jbpm"                      : [],
         "kie-jpmml-integration"     : [],
         "droolsjbpm-integration"    : [],
-        //"droolsjbpm-tools"          : [], // no other repo depends on droolsjbpm-tools
+        "droolsjbpm-tools"          : [],
         "kie-uberfire-extensions"   : [],
         "kie-wb-playground"         : [],
         "kie-wb-common"             : [],
