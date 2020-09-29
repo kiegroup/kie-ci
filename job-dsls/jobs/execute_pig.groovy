60dcab4b40c8e62765669b7fcddf136ec49a83cc
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2020-09-29 14:20:43 +0200

    Revert "Fix 7.44.x (#816)" (#818)
    This reverts commit c0bd43be86d3bec8a5c367ee8e948be76392bc09.

---
+++ job-dsls/jobs/execute_pig.groovy
@@ -0,0 +1,71 @@
+ /**
+ * Executes PiG tool
+ */
+import org.kie.jenkins.jobdsl.Constants
+
+def final DEFAULTS = [
+        repoFolder             : "/home/jenkins/git-repos/build-configuration.git",
+        productFileGenerator   : " /opt/tools/pig/product-files-generator.jar"        
+]
+Closure<Object> get = { String key -> DEFAULTS[key] }
+String repoFolder = get("repoFolder")
+def javadk=Constants.JDK_VERSION
+
+// Creation of folders where jobs are stored
+folder(Constants.DEPLOY_FOLDER)
+
+// jobs for master branch don't use the branch in the name
+String jobName = "pigExecution"
+
+job(jobName) {
+
+    description("PiG execution")
+
+    logRotator {
+        numToKeep(10)
+    }
+
+    parameters {
+        stringParam("repo", "https://gitlab.cee.redhat.com/middleware/build-configurations.git", "the build configuration repo")
+        stringParam("repoBranch", "master", "the build configuration branch to be checked out")
+        stringParam("buildConfiguration", "rhba/7.5.1", "the build configuration folder")
+        stringParam("scmRevision", "", "the revision `sync-7.26.x-2019.10.29` for instance")
+        stringParam("additionalParameters", "--skipBuilds --skipPncUpdate", "")
+    }
+
+    scm {
+        git {
+            remote {
+                url("\${repo}")
+                branch("\${repoBranch}")
+            }
+            extensions {
+                relativeTargetDirectory('git-repos/build-configurations.git')
+            }
+        }
+    }
+    wrappers {
+            configFiles{
+            file('rhba-pnc-cli.conf') {
+                targetLocation('~/.config/pnc-cli/pnc-cli.conf')
+            }
+        }
+    }
+
+    properties {
+        ownership {
+            primaryOwnerId("mbiarnes")
+            coOwnerIds("almorale", "anstephe", "emingora")
+        }
+    }
+
+    label("kie-linux&&kie-mem4g")
+
+    jdk("kie-jdk1.8")
+
+    steps {
+        shell("mkdir ~/.config/pnc-cli -p")
+        shell("cp \$WORKSPACE/~/.config/pnc-cli/pnc-cli.conf ~/.config/pnc-cli/pnc-cli.conf")
+        shell("java -DskipBranchCheck -jar /opt/tools/pig/product-files-generator.jar -c \$WORKSPACE/git-repos/build-configurations.git/\${buildConfiguration} -v scmRevision=\${scmRevision} \${additionalParameters}")
+    }
+}
