0fbf8a7938ac7458d7fdb5d717275ef53e009b07
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2020-09-29 11:36:02 +0200

    branched to 7.44.x

--- job-dsls/jobs/downstream_pr_jobs.groovy
+++ job-dsls/jobs/downstream_pr_jobs.groovy
@@ -138,9 +138,6 @@ for (repoConfig in REPO_CONFIGS) {
                     ghprbBranch {
                         branch("${repoBranch}")
                     }
-                    ghprbBranch {
-                        branch("7.x")
-                    }
                 }
                 useGitHubHooks(true)
                 permitAll(false)
