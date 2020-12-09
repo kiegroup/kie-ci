482a4944bc0eb37173f32a5cc22088deb46c9b8e
Tristan Radisson <tristan.radisson@gmail.com>
2020-12-09 16:07:19 +0100

    Setup shared libs pr job (#875)

---
+++ job-dsls/jobs/jenkins_shared_libs.groovy
@@ -0,0 +1,100 @@
+import org.kie.jenkins.jobdsl.Constants
+
+String ghOrgUnit = Constants.GITHUB_ORG_UNIT
+String ghAuthTokenId = 'kie-ci-token'
+String ghJenkinsfilePwd = 'kie-ci'
+
+// Creation of folders where jobs are stored
+folder(Constants.PULL_REQUEST_FOLDER) {
+    displayName(Constants.PULL_REQUEST_FOLDER_DISPLAY_NAME)
+}
+
+String repository = 'jenkins-pipeline-shared-libraries'
+String repoUrl = "https://github.com/${ghOrgUnit}/${repository}/"
+String jobName = "${Constants.PULL_REQUEST_FOLDER}/${repository}.pr"
+
+pipelineJob(jobName) {
+    description("""Created automatically by Jenkins job DSL plugin. Do not edit manually! The changes will be lost next time the job is generated.
+                |
+                |Every configuration change needs to be done directly in the DSL files. See the below listed 'Seed job' for more info.
+                |""".stripMargin())
+
+    logRotator {
+        numToKeep(10)
+    }
+
+    properties {
+        githubProjectUrl(repoUrl)
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                gitSCM {
+                    userRemoteConfigs {
+                        userRemoteConfig {
+                            url("https://github.com/\${ghprbPullAuthorLogin}/${repository}/")
+                            credentialsId(ghJenkinsfilePwd)
+                            name('')
+                            refspec('')
+                        }
+                    }
+                    branches {
+                        branchSpec {
+                            name('${ghprbSourceBranch}')
+                        }
+                    }
+                    browser { }
+                    doGenerateSubmoduleConfigurations(false)
+                    gitTool('')
+                }
+            }
+            scriptPath('.jenkins/Jenkinsfile')
+        }
+    }
+
+    triggers {
+        ghprbTrigger {
+            // Ordered by appearence in Jenkins UI
+            gitHubAuthId(ghAuthTokenId)
+            adminlist('')
+            useGitHubHooks(true)
+            triggerPhrase('.*[j|J]enkins,?.*(retest|test) this.*')
+            onlyTriggerPhrase(false)
+            autoCloseFailedPullRequests(false)
+            skipBuildPhrase(".*\\[skip\\W+ci\\].*")
+            displayBuildErrorsOnDownstreamBuilds(false)
+            cron('')
+            whitelist(ghOrgUnit)
+            orgslist(ghOrgUnit)
+            blackListLabels('')
+            whiteListLabels('')
+            allowMembersOfWhitelistedOrgsAsAdmin(true)
+            buildDescTemplate('')
+            blackListCommitAuthor('')
+            whiteListTargetBranches{
+                ghprbBranch {
+                    branch('master')
+                }
+            }
+            blackListTargetBranches{}
+            includedRegions('')
+            excludedRegions('')
+            extensions {
+                ghprbSimpleStatus {
+                    commitStatusContext('Build&Test')
+                    addTestResults(true)
+                    showMatrixStatus(false)
+                    statusUrl('${BUILD_URL}display/redirect')
+                    triggeredStatus('')
+                    startedStatus('')
+                }
+            }
+            permitAll(false)
+            commentFilePath('')
+            msgSuccess('Success')
+            msgFailure('Failure')
+            commitStatusContext('')
+        }
+    }
+}
