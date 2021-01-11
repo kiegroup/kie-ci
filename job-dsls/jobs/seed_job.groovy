d1fc3129775f3226c65db8620e62a3aedac035d1
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-01-11 14:05:26 +0100

    removed JDK11 daily build pipeline (#904)

--- job-dsls/jobs/seed_job.groovy
+++ job-dsls/jobs/seed_job.groovy
@@ -58,7 +58,6 @@ job("a-seed-job") {
                     "job-dsls/jobs/**/upstream.groovy\n" +
                     "job-dsls/jobs/**/dailyBuild_pipeline.groovy\n" +
                     "job-dsls/jobs/**/dailyBuild_prod_pipeline.groovy\n" +
-                    "job-dsls/jobs/**/dailyBuild_jdk11_pipeline.groovy\n" +
                     "job-dsls/jobs/**/deploy_jobs.groovy\n" +
                     "job-dsls/jobs/**/kie_jenkinsScripts_PR.groovy\n" +
                     "job-dsls/jobs/**/kie_docs_pr.groovy\n" +
