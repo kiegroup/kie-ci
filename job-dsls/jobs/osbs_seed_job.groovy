f0d14b950925652de1b0afa7437ca8559922e620
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2021-02-04 11:13:35 +0100

    added new seed jobs

---
+++ job-dsls/jobs/osbs_seed_job.groovy
@@ -0,0 +1,73 @@
+import org.kie.jenkins.jobdsl.Constants
+
+// definition of parameters
+
+def javaToolEnv="KIE_JDK1_8"
+def kieMainBranch=Constants.BRANCH
+def organization=Constants.GITHUB_ORG_UNIT
+def javadk=Constants.JDK_VERSION
+
+// +++++++++++++++++++++++++++++++++++++++++++ create a seed job ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// create seed job script
+
+def seedJob='''#!/bin/bash -e
+cd job-dsls
+./gradlew clean test'''
+
+// creation of folder
+folder("OSBS")
+
+def folderPath="OSBS"
+
+job("${folderPath}/osbs-seed-job") {
+
+    description("this job creates all needed Jenkins jobs")
+
+    label("kie-rhel7&&kie-mem8g")
+
+    logRotator {
+        numToKeep(5)
+    }
+
+    jdk("${javadk}")
+
+    scm {
+        git {
+            remote {
+                github("${organization}/kie-jenkins-scripts")
+            }
+            branch ("${kieMainBranch}")
+        }
+    }
+
+    triggers {
+        scm('H/15 * * * *')
+    }
+
+    wrappers {
+        timestamps()
+        colorizeOutput()
+        toolenv("${javaToolEnv}")
+        preBuildCleanup()
+    }
+
+    steps {
+        shell(seedJob)
+
+        jobDsl {
+            targets("job-dsls/jobs/**/prod_seed_cloud_jobs.groovy")
+            useScriptText(false)
+            sandbox(false)
+            ignoreExisting(false)
+            ignoreMissingFiles(false)
+            failOnMissingPlugin(true)
+            unstableOnDeprecation(true)
+            removedJobAction('IGNORE')
+            removedViewAction('DELETE')
+            //removedConfigFilesAction('IGNORE')
+            lookupStrategy('SEED_JOB')
+            additionalClasspath("job-dsls/src/main/groovy")
+        }
+    }
+}
