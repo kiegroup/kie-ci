cd3d7da075758fbfa90b42c6c14726c18ebdf732
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-02-12 13:17:10 +0100

    New dsl for migration (#926)
    * added DSL for rhba-prod-replace-version.org.kie
    
    * added DSL for rhba-prod-branch
    
    * added DSL for jenkins-pipeline-shared-libraries-newBranch
    
    * added DSL for replace_shared_libraries.groovy
    
    * added DSL for slave_image_build_PR_test.groovy
    
    * changed git config not to point to personal account

---
+++ job-dsls/jobs/replace_shared_libraries.groovy
@@ -0,0 +1,97 @@
+def replaceShared = '''
+@Library('jenkins-pipeline-shared-libraries')_
+
+pipeline {
+    agent {
+        label 'kie-rhel7 && !master'
+    }
+    options {
+        buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')
+        timeout(time: 20, unit: 'MINUTES')
+    }
+    stages {
+        stage('Initialize') {
+            steps {
+                sh 'printenv'
+
+            }
+        }
+        stage('Replace projects') {
+            steps {
+                script {
+                    def sharedLibrariesProjects = ['kiegroup/droolsjbpm-build-bootstrap': ['Jenkinsfile', 'Jenkinsfile.nightly'], 'jboss-integration/rhba' : ['Jenkinsfile']]
+                    sharedLibrariesProjects.each { key, val ->
+                        println "[INFO] Treating project ${key}"
+                        def projectGroupName = util.getProjectGroupName(key) 
+                        dir("${projectGroupName[0]}_${projectGroupName[1]}") {
+                            withCredentials([usernamePassword(credentialsId: 'kie-ci', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                                def encodedPass = URLEncoder.encode(PASS, 'UTF-8')
+                                sh "git clone -b ${BRANCH_NAME} https://${USER}:${encodedPass}@github.com/${key} ."
+                                
+                                val.each { file -> 
+                                    println "[INFO] Treating file ${file}"
+                                    sh """
+                                    sed -i "/^@Library('jenkins-pipeline-shared-libraries')_/c\\\\@Library('jenkins-pipeline-shared-libraries@${BRANCH_NAME}')_" ${file}
+                                    """
+                                    sh "git add ${file}"
+                                }
+                                
+                                def changes = sh(returnStdout: true, script: 'git status -s').split()
+                                println "changes ${changes}"
+                                if(changes.size() > 0) {
+                                    println 'There are changes'
+                                    sh 'git config user.email "kie-ci1@redhat.com"'
+                                    sh 'git config user.name "kie-ci1"'
+                                    sh "git commit -m 'pipelines shared libraries import pointing to updated to ${BRANCH_NAME}'"
+                                    sh 'git push origin ${BRANCH_NAME}'
+                                } else {
+                                    println 'No changes, so, nothing to push'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
+'''
+
+// Creation of folders where jobs are stored
+folder("PROD")
+def folderPath="PROD"
+
+// job name
+String jobName = "$folderPath/replace-jenkins-pipeline-shared-libraries"
+
+pipelineJob(jobName) {
+
+    description("""Created automatically by Jenkins job DSL plugin. Do not edit manually! The changes will be lost next time the job is generated.
+                    |
+                    |Every configuration change needs to be done directly in the DSL files.
+                    |
+                    |Will update Jenkinsfile pointing to the new shared-libraries version
+                    |
+                    |""".stripMargin())
+
+    parameters {
+        stringParam ("BRANCH_NAME","7.48.x","The community projects branch")
+    }
+
+    logRotator {
+        numToKeep(10)
+    }
+
+    definition {
+        cps {
+            script("${replaceShared}")
+            sandbox()
+        }
+    }
+
+}
---
+++ job-dsls/jobs/rhba_prod_branch.groovy
@@ -0,0 +1,92 @@
+import org.kie.jenkins.jobdsl.Constants
+
+def newBranch = '''
+@Library('jenkins-pipeline-shared-libraries')_
+
+pipeline {
+    agent {
+        label 'kie-rhel7 && !master'
+    }
+    options {
+        buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')
+        timeout(time: 10, unit: 'MINUTES')
+    }
+    stages {
+        stage('Initialize') {
+            steps {
+                sh 'printenv'
+
+            }
+        }
+        stage('Replace projects') {
+            steps {
+                script {
+                    println "Creating new branch ${BRANCH_NAME}."
+
+                    configFileProvider([configFile(fileId: "49737697-ebd6-4396-9c22-11f7714808eb", variable: 'PRODUCTION_PROJECT_LIST')]) {
+                        println "Reading file ${PRODUCTION_PROJECT_LIST} jenkins file"
+                        def productionProjectListFile = readFile "${env.PRODUCTION_PROJECT_LIST}"
+                        def projectCollection = productionProjectListFile.readLines()
+                        println "Project Collection ${projectCollection}."
+                        
+                        projectCollection.each { project ->
+                            def projectGroupName = util.getProjectGroupName(project)
+                            def group = projectGroupName[0]
+                            def name = projectGroupName[1]
+                            dir("${env.WORKSPACE}/${group}_${name}") {
+                                withCredentials([usernamePassword(credentialsId: 'kie-ci', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                                    def encodedPass = URLEncoder.encode(PASS, 'UTF-8')
+                                    sh "git clone https://${USER}:${encodedPass}@github.com/${group}/${name} ."
+                                    sh "git checkout -b ${BRANCH_NAME}"
+                                    sh 'git config user.email "kie-ci1@redhat.com"'
+                                    sh 'git config user.name "kie-ci1"'
+                                    sh "git push origin ${BRANCH_NAME}"
+                                }
+                            }                        
+                        }
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
+'''
+
+// Creation of folders where jobs are stored
+folder("PROD")
+def folderPath="PROD"
+
+// job name
+String jobName = "$folderPath/rhba-prod-branch"
+
+pipelineJob(jobName) {
+
+    description("""Created automatically by Jenkins job DSL plugin. Do not edit manually! The changes will be lost next time the job is generated.
+                    |
+                    |Every configuration change needs to be done directly in the DSL files.
+                    |
+                    |This job has to be run when a new branch for prod is required.
+                    |
+                    |""".stripMargin())
+
+    parameters {
+        stringParam ("BRANCH_NAME","7.48.x","The new branch to create")
+    }
+
+    logRotator {
+        numToKeep(10)
+    }
+
+    definition {
+        cps {
+            script("${newBranch}")
+            sandbox()
+        }
+    }
+
+}
