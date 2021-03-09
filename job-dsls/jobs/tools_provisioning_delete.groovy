3b0b30f87d0818fdbc745e10c3fd88836ec2292c
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-03-09 14:51:09 +0100

    Tools provisioning (#940)
    * added DSL groovy scripts for delete jobs in Tools/Provisioning
    
    * added DSL groovy scripts for provision jobs in Tools/Provisioning
    
    * added DSL groovy scripts for provision jobs in Tools/Provisioning
    
    * refactored Tools/Provisioning DSL scripts
    
    * added provisioning job for cekit-cacher
    
    * added provisioning job for docker-reg
    
    * added provisioning job for smee-client
    
    * added provisioning job for verdacio-service
    
    * removed obsolete parameters
    
    * removed initial tools-provisioning job

---
+++ job-dsls/jobs/tools_provisioning_delete.groovy
@@ -0,0 +1,165 @@
+/**
+ * Creates all detele-jobs in Jenkins/Tools/provisioning
+ * delete-cekit-cacher
+ * delete-docker-registry
+ * delete-smee-client
+ * delete-verdaccio-service
+ */
+
+def final DEFAULTS = [
+        jobAbr : "",
+        folderPath : "Provisioning",
+        logRot : 10,
+        labExp : "ansible",
+        timeOutVar : 30,
+        param : "",
+        paramDescription: "",
+        jobDescription: ""
+]
+def final JOB_NAMES = [
+        "delete-cekit-cacher"       : [
+                jobAbr: "CekitCacher",
+                jobDescription : "Destroys cekit-cacher machine from PSI (Upshift) OpenStack.<br>Do NOT run unless you know what you are doing!"
+        ],
+        "delete-docker-registry"    : [
+                jobAbr: "DockerReg",
+                jobDescription : "Destroys local docker-registry machine from PSI (Upshift) OpenStack.<br>Do NOT run unless you know what you are doing!"
+        ],
+        "delete-smee-client"        : [
+                jobAbr: "SmeeClient",
+                jobDescription : "Destroys smee-client machine from PSI (Upshift) OpenStack.<br>Do NOT run unless you know what you are doing!",
+                param : "SMEE_NUMBER",
+                paramDescription : "The number related with the BUILD_ID from provision-smee-client job when the machine was provisioned. 14 for instance"
+        ],
+        "delete-verdaccio-service"  : [
+                jobAbr: "VerdaccioServ",
+                jobDescription : "Destroys verdaccio-service machine from PSI (Upshift) OpenStack.<br>Do NOT run unless you know what you are doing!",
+                param: "INSTANCE_NUMBER",
+                paramDescription: "The number related with the BUILD_ID from provision-verdaccio-service job when the machine was provisioned. 14 for instance"
+        ]
+]
+
+//create folders
+folder("Provisioning")
+
+for (jobNames in JOB_NAMES) {
+    Closure<Object> get = { String key -> jobNames.value[key] ?: DEFAULTS[key] }
+
+    String jobName = jobNames.key
+    String folderPath = get("folderPath")
+    String jobAbr = get("jobAbr")
+    String labExp = get("labExp")
+    String jobDescription = get('jobDescription')
+    String param = get("param")
+    String paramDesc = get("paramDescription")
+    def logRot = get("logRot")
+    def timeOutVar = get("timeOutVar")
+
+    String fullName = getFullName(jobAbr)
+
+    // jobs for master branch don't use the branch in the name
+    String jobN = "$folderPath/$jobName"
+
+    job(jobN) {
+        description(jobDescription)
+
+        logRotator {
+            numToKeep(logRot)
+        }
+
+        label(labExp)
+
+        if ( param != "") {
+            parameters {
+                stringParam ("${param}","","${paramDesc}")
+            }
+        }
+
+        // Adds pre/post actions to the job.
+        wrappers {
+
+            // Adds timestamps to the console log.
+            timestamps()
+
+            // Adds timeout
+            timeout{
+                absolute(timeOutVar)
+            }
+
+            // Renders ANSI escape sequences, including color, to console output.
+            colorizeOutput()
+
+            // Deletes files from the workspace before the build starts.
+            preBuildCleanup()
+
+            // Loads file provider
+            configFileProvider {
+                managedFiles {
+                    configFile {
+                        fileId('openstack-upshift-rc-file')
+                        targetLocation('\$WORKSPACE/openrc.sh')
+                        variable('OPENRC_FILE')
+                    }
+                }
+            }
+
+            // Injects passwords as environment variables into the job.
+            injectPasswords {
+            // Injects global passwords provided by Jenkins configuration.
+                injectGlobalPasswords(true)
+                // Masks passwords provided by build parameters.
+                maskPasswordParameters(true)
+            }
+
+
+        }
+
+        // Adds custom properties to the job.
+        properties {
+
+            // Allows to configure job ownership.
+            ownership {
+
+                // Sets the name of the primary owner of the job.
+                primaryOwnerId("mbiarnes")
+
+                // Adds additional users, who have ownership privileges.
+                coOwnerIds("anstephe", "mnovotny", "almorale")
+            }
+        }
+
+        // Adds publishrs o job
+        publishers {
+            wsCleanup()
+        }
+
+        // Adds step that executes a shell script
+        steps {
+            shell(getScript(fullName))
+        }
+    }
+}
+
+def getFullName(String jobAbr) {
+    switch (jobAbr) {
+        case "VerdaccioServ":
+            return "\"verdaccio-service-\$INSTANCE_NUMBER\""
+        case "DockerReg":
+            return "\"provisioner-job-docker-registry-server\""
+        case "SmeeClient":
+            return "\"smee-client-\$SMEE_NUMBER\""
+        default:
+            return "\"provisioner-job-cekit-cacher-server\""
+    }
+}
+
+def getScript(String fullName) {
+    return ". \$OPENRC_FILE\n" +
+            "# OS_PASSWORD gets injected from global passwords\n" +
+            "\n" +
+            "export FULL_NAME=$fullName\n" +
+            "\n" +
+            "# delete the machine\n" +
+            "openstack server delete \$FULL_NAME\n" +
+            "sleep 10s\n"
+}
---
+++ job-dsls/jobs/tools_seed_job.groovy
@@ -0,0 +1,71 @@
+import org.kie.jenkins.jobdsl.Constants
+
+// definition of parameters
+
+def javaToolEnv="KIE_JDK1_8"
+def kieMainBranch=Constants.BRANCH
+def organization=Constants.GITHUB_ORG_UNIT
+def javadk=Constants.JDK_VERSION
+
+// +++++++++++++++++++++++++++++++++++++++++++ create a seed job ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// create seed job script
+
+def seedJob='''#!/bin/bash -e
+cd job-dsls
+./gradlew clean test'''
+
+job("a-tools-seed-job") {
+
+    description("this job creates all needed Jenkins jobs")
+
+    label("kie-rhel7")
+
+    logRotator {
+        numToKeep(5)
+    }
+
+    jdk("${javadk}")
+
+    scm {
+        git {
+            remote {
+                github("${organization}/kie-jenkins-scripts")
+            }
+            branch ("${kieMainBranch}")
+        }
+    }
+
+    /*triggers {
+        scm('H/15 * * * *')
+    }*/
+
+    wrappers {
+        timestamps()
+        colorizeOutput()
+        toolenv("${javaToolEnv}")
+        preBuildCleanup()
+    }
+
+    steps {
+        shell(seedJob)
+
+        jobDsl {
+            targets("job-dsls/jobs/**/slave_image_build.groovy\n" +
+                    "job-dsls/jobs/**/slave_image_build_PR_test.groovy\n" +
+                    "job-dsls/jobs/**/tools_provisioning*.groovy\n" +
+                    "job-dsls/jobs/**/tools_seed_job.groovy")
+            useScriptText(false)
+            sandbox(false)
+            ignoreExisting(false)
+            ignoreMissingFiles(false)
+            failOnMissingPlugin(true)
+            unstableOnDeprecation(true)
+            removedJobAction('IGNORE')
+            removedViewAction('DELETE')
+            //removedConfigFilesAction('IGNORE')
+            lookupStrategy('SEED_JOB')
+            additionalClasspath("job-dsls/src/main/groovy")
+        }
+    }
+}
