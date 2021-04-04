c676cd8ea29e625c442973425ed504368c91d2fd
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-04-04 10:59:58 +0200

    fixed seed jobs (#958)

--- job-dsls/jobs/seed_jobs/tools_seed_job.groovy
+++ job-dsls/jobs/seed_jobs/tools_seed_job.groovy
@@ -55,7 +55,8 @@ job("${folderPath}/a-seed-job-tools") {
             removedViewAction('IGNORE')
             removedConfigFilesAction('IGNORE')
             lookupStrategy('JENKINS_ROOT')
-            additionalClasspath("job-dsls/src/main/groovy,job-dsls/src/main/resources")
+            additionalClasspath("job-dsls/src/main/groovy\n" +
+                    "job-dsls/src/main/resources")
         }
     }
 }
