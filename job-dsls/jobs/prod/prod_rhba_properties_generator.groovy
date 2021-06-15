6a0536304cb9e0d6c08d44ae71cd4eb14b522eee
Alberto Morales Perez <almorale@redhat.com>
2021-06-15 11:20:54 +0100

    Add a symlink to the latest nightly properties so it can be referenced without specifying a date
    Signed-off-by: Alberto Morales Perez <almorale@redhat.com>

--- job-dsls/jobs/prod/prod_rhba_properties_generator.groovy
+++ job-dsls/jobs/prod/prod_rhba_properties_generator.groovy
@@ -33,15 +33,26 @@ node('kie-rhel7&&!master') {
     ]
     if(Boolean.valueOf(IS_PROD)) {
         // RHPAM
-        publishFile("6ad7aff1-2d3d-4cdc-81de-b62dae1f39e9", "rhpam-deliverable-list-staging.properties", binding, "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}")
-        publishFile("f5eb870f-53d8-426c-bcfa-04668965e3ef", "rhpam-deliverable-list.properties", binding, "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}")
+        def rhpamFolder = "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}"
+        publishFile("6ad7aff1-2d3d-4cdc-81de-b62dae1f39e9", "rhpam-deliverable-list-staging.properties", binding, rhpamFolder)
+        publishFile("f5eb870f-53d8-426c-bcfa-04668965e3ef", "rhpam-deliverable-list.properties", binding, rhpamFolder)
 
         // RHDM
-        publishFile("8862cf74-d316-4eea-a99e-f74d90be6931", "rhdm-deliverable-list-staging.properties", binding, "${RCM_GUEST_FOLDER}/rhdm/RHDM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}")
-        publishFile("598bedb7-780f-4f46-994f-e6314d55d8b9", "rhdm-deliverable-list.properties", binding, "${RCM_GUEST_FOLDER}/rhdm/RHDM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}")
+        def rhdmFolder = "${RCM_GUEST_FOLDER}/rhdm/RHDM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}"
+        publishFile("8862cf74-d316-4eea-a99e-f74d90be6931", "rhdm-deliverable-list-staging.properties", binding, rhdmFolder)
+        publishFile("598bedb7-780f-4f46-994f-e6314d55d8b9", "rhdm-deliverable-list.properties", binding, rhdmFolder)
     } else {
-        publishFile("aff8076d-3a5d-4e45-b41e-413ca9b34258", "rhpam-${TIME_STAMP}.properties", binding, "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.NIGHTLY")
-        publishFile("8196c1f9-71ee-4bb1-8244-6b7711715c66", "rhdm-${TIME_STAMP}.properties", binding, "${RCM_GUEST_FOLDER}/rhdm/RHDM-${PRODUCT_VERSION}.NIGHTLY")
+        // RHPAM
+        def rhpamFolder = "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.NIGHTLY"
+        def rhpamProperties = "rhpam-${TIME_STAMP}.properties"
+        publishFile("aff8076d-3a5d-4e45-b41e-413ca9b34258", rhpamProperties, binding, rhpamFolder)
+        publishSymlink("${rhpamFolder}/${rhpamProperties}", "${rhpamFolder}/rhpam-latest.properties")
+
+        // RHDM
+        def rhdmFolder = "${RCM_GUEST_FOLDER}/rhdm/RHDM-${PRODUCT_VERSION}.NIGHTLY"
+        def rhdmProperties = "rhdm-${TIME_STAMP}.properties"
+        publishFile("8196c1f9-71ee-4bb1-8244-6b7711715c66", rhdmProperties, binding, rhdmFolder)
+        publishSymlink("${rhdmFolder}/${rhdmProperties}", "${rhdmFolder}/rhdm-latest.properties")
     }
 }
 
@@ -65,6 +76,12 @@ def publishFile(String fileId, String fileName, Map<String, String> binding, Str
     sshagent(credentials: ['rcm-publish-server']) {
         sh "ssh 'rhba@${RCM_HOST}' 'mkdir -p ${folder}'"
         sh "scp -o StrictHostKeyChecking=no ${fileName} rhba@${RCM_HOST}:${folder}"
+    }
+}
+
+def publishSymlink(String symlink, String target) {
+    sshagent(credentials: ['rcm-publish-server']) {
+        sh "ssh 'rhba@${RCM_HOST}' 'ln -sf ${target} ${symlink}'"
     }
 }
 '''
