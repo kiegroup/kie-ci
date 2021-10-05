573445a4acc93913c3c8614a6aba11ba4e86e081
Enrique Mingorance Cano <emingora@redhat.com>
2021-10-05 16:37:43 +0200

    [BXMSPROD-1505] properties_generator.groovy (#1128)
    * [BXMSPROD-1505] properties_generator.groovy
    
    * import statement moved to the top of the pipeline
    
    * JsonSlurperClassic instead of JsonSlurper

--- job-dsls/jobs/prod/prod_rhdm_properties_generator.groovy
+++ job-dsls/jobs/prod/prod_rhdm_properties_generator.groovy
@@ -2,22 +2,25 @@
  * Generate properties files for nightly and productized builds.
  *
  * Generated files for nightly:
- * - {rhpam}-{timestamp}.properties - properties file pointing to nightly binaries in the staging area
+ * - {rhdm}-{timestamp}.properties - properties file pointing to nightly binaries in the staging area
  *
  * Generated files for productized builds:
- * - {rhpam}-deliverable-list-staging.properties - properties file pointing binaries in the staging area
- * - {rhpam}-deliverable-list.properties - properties file pointing binaries in the candidates area
+ * - {rhdm}-deliverable-list-staging.properties - properties file pointing binaries in the staging area
+ * - {rhdm}-deliverable-list.properties - properties file pointing binaries in the candidates area
  */
 
 def propGen ='''
+import groovy.json.JsonOutput
+
 node('kie-rhel7&&!master') {
+
     sh 'env\'
     def REPO_URL_FOLDER_VERSION = 'main'.equals(BRANCH_NAME) ? 'main' : (PRODUCT_VERSION =~ /\\d+\\.\\d+/)[0]
     println "Folder [${REPO_URL_FOLDER_VERSION}] based on BRANCH_NAME [${BRANCH_NAME}] and PRODUCT_VERSION [${PRODUCT_VERSION}]"
     def REPO_URL_FINAL = REPO_URL.replace("-main-", "-${REPO_URL_FOLDER_VERSION}-")
     println "REPO_URL_FINAL [${REPO_URL_FINAL}]"
 
-    def binding = [
+    def binding = JsonOutput.toJson([
             "REPO_URL"                   : REPO_URL_FINAL,
             "DELIVERABLE_REPO_URL"       : DELIVERABLE_REPO_URL,
             "PRODUCT_VERSION"            : PRODUCT_VERSION,
@@ -30,36 +33,31 @@ node('kie-rhel7&&!master') {
             "IZPACK_VERSION"             : IZPACK_VERSION,
             "INSTALLER_COMMONS_VERSION"  : INSTALLER_COMMONS_VERSION,
             "JAVAPARSER_VERSION"         : JAVAPARSER_VERSION
-    ]
-    if(Boolean.valueOf(IS_PROD)) {
-        // RHPAM
-        publishFile("6ad7aff1-2d3d-4cdc-81de-b62dae1f39e9", "rhpam-deliverable-list-staging.properties", binding, "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}")
-        publishFile("f5eb870f-53d8-426c-bcfa-04668965e3ef", "rhpam-deliverable-list.properties", binding, "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}")
+    ])
+    if(Boolean.valueOf(IS_RELEASE)) {
+        def folder = "rhdm/RHDM-${PRODUCT_VERSION}.${PRODUCT_MILESTONE}"
+
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [$class: 'StringParameterValue', name: 'FILE_ID', value: '8862cf74-d316-4eea-a99e-f74d90be6931'],
+            [$class: 'StringParameterValue', name: 'FILE_NAME', value: 'rhdm-deliverable-list-staging.properties'],
+            [$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [$class: 'StringParameterValue', name: 'FILE_ID', value: '598bedb7-780f-4f46-994f-e6314d55d8b9'],
+            [$class: 'StringParameterValue', name: 'FILE_NAME', value: 'rhdm-deliverable-list.properties'],
+            [$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
     } else {
-        publishFile("aff8076d-3a5d-4e45-b41e-413ca9b34258", "rhpam-${TIME_STAMP}.properties", binding, "${RCM_GUEST_FOLDER}/rhpam/RHPAM-${PRODUCT_VERSION}.NIGHTLY")
-    }
-}
+        def folder = "rhdm/RHDM-${PRODUCT_VERSION}.NIGHTLY"
 
-def replaceTemplate(String fileId, Map<String, String> binding) {
-    println "Replace Template ${fileId}"
-    def content = ""
-    configFileProvider([configFile(fileId: fileId, variable: 'PROPERTIES_FILE')]) {
-        content = readFile "${env.PROPERTIES_FILE}"
-        for (def bind : binding) {
-            content = content.replace("\\${" + bind.getKey() + "}", bind.getValue().toString())
-        }
-    }
-    return content
-}
-
-def publishFile(String fileId, String fileName, Map<String, String> binding, String folder) {
-    println "Publishing [${fileId}], name [${fileName}] into folder [${folder}] ..."
-    def content = replaceTemplate(fileId, binding)
-    println content
-    writeFile file: "${fileName}", text: content
-    sshagent(credentials: ['rcm-publish-server']) {
-        sh "ssh 'rhba@${RCM_HOST}' 'mkdir -p ${folder}'"
-        sh "scp -o StrictHostKeyChecking=no ${fileName} rhba@${RCM_HOST}:${folder}"
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [$class: 'StringParameterValue', name: 'FILE_ID', value: '8196c1f9-71ee-4bb1-8244-6b7711715c66'],
+            [$class: 'StringParameterValue', name: 'FILE_NAME', value: "rhdm-${TIME_STAMP}.properties"],
+            [$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
     }
 }
 '''
@@ -67,11 +65,11 @@ def publishFile(String fileId, String fileName, Map<String, String> binding, Str
 folder("PROD")
 def folderPath = "PROD"
 
-pipelineJob("${folderPath}/rhba-properties-generator") {
-    description("Generate properties files for nightly and productized builds")
+pipelineJob("${folderPath}/rhdm-properties-generator") {
+    description("Generate properties files for RHDM nightly and productized builds")
 
     parameters {
-        booleanParam("IS_PROD", true, "it defines if the properties file is for prod or not")
+        booleanParam("IS_RELEASE", true, "it defines if the properties file is for release or not")
         stringParam("BRANCH_NAME", "main", "the branch the nightly was triggered for")
         stringParam("REPO_URL", "http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/content/repositories/rhba-main-nightly", "Prod possibility is http://download.devel.redhat.com/rcm-guest/staging/")
         stringParam("DELIVERABLE_REPO_URL", "http://download.devel.redhat.com/devel/candidates")
@@ -85,8 +83,6 @@ pipelineJob("${folderPath}/rhba-properties-generator") {
         stringParam("IZPACK_VERSION")
         stringParam("INSTALLER_COMMONS_VERSION")
         stringParam("JAVAPARSER_VERSION", "", "This is just for prod files")
-        stringParam("RCM_GUEST_FOLDER", "/mnt/rcm-guest/staging")
-        stringParam("RCM_HOST", "rcm-guest.app.eng.bos.redhat.com")
     }
 
     definition {
