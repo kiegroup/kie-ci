3f62a23866683a03ab977f0f962c4649ec2fcf06
ginxo <emingora@redhat.com>
2021-10-05 14:34:05 +0200

    kogito 1.11.x to take rhba artifacts from main repo

--- job-dsls/jobs/prod/prod_cron_meta_nightly_pipeline.groovy
+++ job-dsls/jobs/prod/prod_cron_meta_nightly_pipeline.groovy
@@ -80,7 +80,7 @@ pipeline{
         stage('trigger kogito nightly job ${KOGITO_CURRENT_PRODUCT_VERSION}') {
             steps {
                 build job: 'kogito.nightly/${KOGITO_CURRENT_PRODUCT_VERSION}', propagate: false, wait: true, parameters: [
-                        [\$class: 'StringParameterValue', name: 'RHBA_MAVEN_REPO_URL', value: 'http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/content/repositories/rhba-${getNexusFromVersion(NEXT_PRODUCT_VERSION)}-nightly-with-upstream'],
+                        [\$class: 'StringParameterValue', name: 'RHBA_MAVEN_REPO_URL', value: 'http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/content/repositories/rhba-${NEXT_PRODUCT_BRANCH}-nightly-with-upstream'],
                         [\$class: 'StringParameterValue', name: 'RHBA_VERSION_PREFIX', value: '${RHBA_VERSION_PREFIX}'],
                         [\$class: 'StringParameterValue', name: 'KOGITO_DEPLOYMENT_REPO_URL', value: 'https://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8443/nexus/service/local/repositories/scratch-release-kogito-${getNexusFromVersion(KOGITO_CURRENT_PRODUCT_VERSION)}/content-compressed'],
                         [\$class: 'StringParameterValue', name: 'UMB_VERSION', value: '${getUMBFromVersion(KOGITO_CURRENT_PRODUCT_VERSION)}'],
