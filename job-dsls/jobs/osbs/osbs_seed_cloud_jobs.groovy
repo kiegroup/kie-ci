4cc7f4b188c3c9208719b48cd803f2d7ac021da9
Mauricio Magnani <msmagnanijr@gmail.com>
2021-11-24 04:32:38 -0300

    [KIECLOUD-600] - Remove RHDM from OSBS Seed Cloud Jobs (#1164)
    See: https://issues.redhat.com/browse/KIECLOUD-600
    
    Signed-off-by: Mauricio Magnani <msmagnanijr@gmail.com>

--- job-dsls/jobs/osbs/osbs_seed_cloud_jobs.groovy
+++ job-dsls/jobs/osbs/osbs_seed_cloud_jobs.groovy
@@ -6,7 +6,7 @@ def folderPath = "OSBS"
 String jobDescription = "Job responsible for seed jobs to building rhpm/rhdm openshift image"
 
 //Define Variables
-def prodComponent = ['rhpam-businesscentral','rhpam-businesscentral-monitoring','rhpam-controller','rhpam-kieserver','rhpam-smartrouter','rhdm-decisioncentral','rhdm-controller','rhdm-kieserver','rhpam-process-migration','rhdm-optaweb-employee-rostering']
+def prodComponent = ['rhpam-businesscentral','rhpam-businesscentral-monitoring','rhpam-controller','rhpam-kieserver','rhpam-smartrouter','rhpam-process-migration']
 
 def buildDate=Constants.BUILD_DATE
 def prodVersion=Constants.NEXT_PROD_VERSION
@@ -26,7 +26,7 @@ def verbose=Constants.VERBOSE
 
 
 prodComponent.each { Component ->
-            
+
    pipelineJob("${folderPath}/${Component}")  {
 
     parameters {
@@ -47,14 +47,14 @@ prodComponent.each { Component ->
         stringParam("CEKIT_CACHE_LOCAL", "${cekitCacheLocal}")
         stringParam("VERBOSE", "${verbose}")
     }
-    
+
   definition {
     cps {
 
         script('''
-  
+
                       private void validateParameters(required, optionals){
-                          // Check if all required params are supplied 
+                          // Check if all required params are supplied
                           for ( param in required ) {
                               def arg = param.value[0]
                               def flag = param.value[1]
@@ -63,16 +63,16 @@ prodComponent.each { Component ->
                               }
                           }
                       }
-               
+
                       private void mainProcess(user, password, keytab){
-  
+
                           """ Parameters available for the build. """
                           def REQUIRED_BUILD_PARAMETERS = [
                                   "PROD_VERSION": [PROD_VERSION, '-v'],
                                   "PROD_COMPONENT": [PROD_COMPONENT, '-c'],
                                   "OSBS_BUILD_TARGET": [OSBS_BUILD_TARGET, '-t'],
                           ]
-                          
+
                           def OPTIONAL_BUILD_PARAMETERS = [
                                   "KERBEROS_PASSWORD": [password, '-s'],
                                   "KERBEROS_PRINCIPAL": [user, '-p'],
@@ -84,29 +84,29 @@ prodComponent.each { Component ->
                                   "CEKIT_BUILD_OPTIONS": [CEKIT_BUILD_OPTIONS, '-o'],
                                   "CEKIT_CACHE_LOCAL": [CEKIT_CACHE_LOCAL, '-l'],
                           ]
-                          
+
                           def OPTIONAL_BUILD_SWITCHES = [
                                   "VERBOSE": [VERBOSE, '-g'],
                           ]
-                          
+
                           def REQUIRED_DOWNLOAD_PARAMETERS = [
                                   "PROD_VERSION": [PROD_VERSION, '-v'],
                                   "PROD_COMPONENT": [PROD_COMPONENT, '-c'],
                           ]
-                          
+
                           def OPTIONAL_DOWNLOAD_PARAMETERS = [
                                   "IMAGE_REPO": [IMAGE_REPO, '-r'],
                                   "IMAGE_BRANCH": [IMAGE_BRANCH, '-n'],
                                   "IMAGE_SUBDIR": [IMAGE_SUBDIR, '-d'],
                           ]
-                          
+
                           // The download script is in the image, but build.sh and build-overrides.sh which it calls will be downloaded
                           String DOWNLOAD_SCRIPT = "/opt/rhba/download.sh"
-                          def download_command = "$DOWNLOAD_SCRIPT"  
-                          
+                          def download_command = "$DOWNLOAD_SCRIPT"
+
                           String BUILD_SCRIPT = "build-osbs.sh"
                           def build_command = "$BUILD_SCRIPT"
-  
+
                           // Create the download command to set up the build directory
                           validateParameters(REQUIRED_DOWNLOAD_PARAMETERS, OPTIONAL_DOWNLOAD_PARAMETERS)
                           for(param in REQUIRED_DOWNLOAD_PARAMETERS){
@@ -114,61 +114,61 @@ prodComponent.each { Component ->
                           def flag = param.value[1]
                           download_command += " ${flag} ${arg}"
                           }
-                          
+
                           for(param in OPTIONAL_DOWNLOAD_PARAMETERS){
                           def arg = param.value[0]
                           def flag = param.value[1]
                           if(arg) download_command+= " ${flag} ${arg}"
                           }
-                          
+
                           download_command += " -w ${WORKSPACE}"
-                          
-                          // Create the build command 
+
+                          // Create the build command
                           validateParameters(REQUIRED_BUILD_PARAMETERS, OPTIONAL_BUILD_PARAMETERS)
-  
+
                           for(param in REQUIRED_BUILD_PARAMETERS){
                           def arg = param.value[0]
                           def flag = param.value[1]
                           build_command += " ${flag} ${arg}"
                           }
-                          
+
                           for(param in OPTIONAL_BUILD_PARAMETERS){
                           def arg = param.value[0]
                           def flag = param.value[1]
                           if(arg) build_command+= " ${flag} ${arg}"
                           }
-  
+
                           for(param in OPTIONAL_BUILD_SWITCHES){
                           def arg = param.value[0]
                           def flag = param.value[1]
                           if(arg == "true") build_command+= " ${flag}"
                           }
-  
+
                           build_command +=" -w ${WORKSPACE}"
-  
+
                           // Run the download script to set up the build. This will select the right branch
                           // and return the component directory path where the build needs to take path
                           def component_path = sh(script: "$download_command 2>&1", returnStdout: true)
                           println component_path
-                          
+
                           // This gets the last token of output from the script, which is the path to build in
                           component_path = component_path.tokenize().last()
-                          
-                          // Run the build script    
+
+                          // Run the build script
                           dir(component_path) {
                               sh "source ~/virtenvs/cekit/bin/activate && $build_command"
                           }
-                          
-                          // post processing 
+
+                          // post processing
                       }
-  
+
                       node("osbs-builder"){
-  
+
                       stage("building ${PROD_COMPONENT}") {
-  
+
                           ws {
                               // Add the working directory to the current path so any scripts dropped there are on the path
-                              withEnv(["PATH+W=$WORKSPACE"]) {  
+                              withEnv(["PATH+W=$WORKSPACE"]) {
                                   sh 'rm -rf ${WORKSPACE}/{*,.*} || true'
                                   if (env.KERBEROS_CRED) {
                                   withCredentials([usernamePassword(credentialsId: env.KERBEROS_CRED, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
@@ -185,9 +185,9 @@ prodComponent.each { Component ->
                             }
                           }
                       }
-  
+
         '''.stripIndent())
-        sandbox()  
+        sandbox()
     }
     }
   }
