61b9ce9f9cd4a41781596acd3e05a54c7fecab8b
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2021-12-23 09:07:11 +0100

    BXMSPROD-1582: remove jenkins-agents and job-dsls/jobs/tools

--- jenkins-agents/add-osbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "usage: add-osbs.sh <the url of the bxms-jenkins git repository>"
-    exit 0
-fi
-git clone $1
-rsync -av bxms-jenkins/jenkins-image-extra-bits/rhba-osbs/ansible/ ansible
