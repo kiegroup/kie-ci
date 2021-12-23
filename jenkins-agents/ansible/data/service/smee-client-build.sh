11f435b1a1b3874db7d90e04ee33985f9b190134
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-12-23 10:54:16 +0100

    BXMSPROD-1582: remove jenkins-agents and job-dsls/jobs/tools (#1183)

--- jenkins-agents/ansible/data/service/smee-client-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# workaround for building the fix from https://github.com/probot/smee-client/pull/162
-# to be removed after the PR ^ is merged and a new smee-client released!!
-
-git clone -b drop-host-header https://github.com/kahowell/smee-client
-cd smee-client
-npm install
-npm run build
