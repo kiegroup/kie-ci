56dfc5aa45084c3bc0aeec67c6c4c963929898e9
domhanak <domin.hanak@gmail.com>
2022-01-06 12:30:12 +0100

    Add kogito-tooling-prelease branch trigger
    Rework the job with generic-webhook-trigger
    
    Add nexesarry logic for version parsing
    
    Fix variables in script, reuse ghAuthTokenId
    
    Escape regex in all places
    
    Add missing testPlugin
    
    Downgrade generic-webhook-trigger to 1.72
    
    Fix regexPFilterValue

---
+++ job-dsls/jobs/kie/kogito/kogito_tooling_prerelease_branch_UMB_trigger.groovy
@@ -0,0 +1,98 @@
+/**
+ * Creates job that triggers when a new `<MAJOR>-<MINOR>-<PATCH>-prerelease` branch is
+   pushed to kogito-tooling repository and notifies QE of this by sending and UMB message.
+ */
+import org.kie.jenkins.jobdsl.Constants
+
+def repo = "kogito-tooling"
+def repoBranch = ''
+def ghOrgUnit = "kiegroup"
+def ghAuthTokenId = "kie-ci"
+def labelName = "kie-rhel7"
+def regexpFilterRegexValue = '([0-9]+)\\.([0-9]+)\\.([0-9]+)-prerelease'
+
+// creation of folder
+folder("KIE")
+folder ("KIE/kogito")
+folder ("KIE/kogito/kogito-tooling")
+
+def folderPath="KIE/kogito/kogito-tooling"
+
+String jobName = "${folderPath}/${repo}-prerelease-branch-UMB-trigger"
+
+pipelineJob(jobName) {
+ parameters {
+  stringParam('ref', '')
+  stringParam('ref_type', '')
+ }
+
+ triggers {
+  genericTrigger {
+   genericVariables {
+    genericVariable {
+     key("ref")
+     value("\$.ref")
+    }
+    genericVariable {
+     key("ref_type")
+     value("\$.ref_type")
+    }
+   }
+   tokenCredentialId(ghAuthTokenId)
+   printContributedVariables(true)
+   printPostContent(true)
+   silentResponse(false)
+   regexpFilterText("\$ref")
+   regexpFilterExpression(regexpFilterRegexValue)
+  }
+ }
+
+ environmentVariables{
+    groovy('''
+        if (ref_type.equals("branch") && ref.endsWith("-prerelease")) {
+            def kogitoToolingBranch = ref
+            def kogitoToolingVersion = (ref =~ /[0-9]+\\.[0-9]+\\.[0-9]+/)[ 0 ]
+
+            println kogitoToolingBranch
+            println kogitoToolingVersion
+
+            def result = ["KOGITO_TOOLING_BRANCH":  kogitoToolingBranch, "KOGITO_TOOLING_VERSION": kogitoToolingVersion]
+            return result;
+        } else {
+            return null;
+        }
+    ''')
+ }
+
+ definition {
+  cps {
+   script('''
+    node {
+     stage('Send UMB') {
+         ciMessageBuilder {
+            providerData {
+                activeMQPublisher {
+                    name('Red Hat Umb')
+                    messageContent('
+                                    {
+                                        \"npmRegistry\": \"\${NPM_REGISTRY_PUBLISH_URL}",
+                                        \"kogitoToolingVersion\": \"\${KOGITO_TOOLING_VERSION}\",
+                                        \"kogitoToolingBranch\": \"\${KOGITO_TOOLING_BRANCH}\"
+                                    }
+                    ')
+                    failOnError(false)
+                    messageProperties('CI_TYPE=custom label=rhba-ci')
+                    messageType('Custom')
+                    overrides {
+                        topic('VirtualTopic.qe.ci.ba.KOGITO_TOOLING.\${KOGITO_TOOLING_VERSION}.CR.trigger')
+                    }
+                }
+            }
+        }  
+     }
+    }
+   ''')
+   sandbox()
+  }
+ }
+}
