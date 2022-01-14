97299c4a8abb54afb5eef17cb9d787a679c6aaff
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2022-01-14 16:32:23 +0100

    removed 7.x branch where specified

--- job-dsls/jobs/kie/main/downstream_pr_jobs.groovy
+++ job-dsls/jobs/kie/main/downstream_pr_jobs.groovy
@@ -156,9 +156,6 @@ for (repoConfig in REPO_CONFIGS) {
                             ghprbBranch {
                                 branch("${repoBranch}")
                             }
-                            ghprbBranch {
-                                branch("7.x")
-                            }
                         }
                         useGitHubHooks(true)
                         permitAll(false)
