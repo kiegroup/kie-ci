89843a003aee55ddaefe95c38f3fa3db6741fd28
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2022-01-27 13:53:36 +0100

    added parameter for product versions (#1200)

--- job-dsls/jobs/kie/kie-tools/upgradeVersions/upgrade_start_jbpm_org.groovy
+++ job-dsls/jobs/kie/kie-tools/upgradeVersions/upgrade_start_jbpm_org.groovy
@@ -2,6 +2,7 @@
 
 def CURRENT_KIE_VERSION = ''
 def NEW_KIE_VERSION= ''
+def PROD_VERSION=''
 def GH_ORG_UNIT = 'kiegroup'
 def JENKINSFILE_REPO = 'start.jbpm.org'
 def JENKINSFILE_PWD= 'kie-ci'
@@ -21,12 +22,14 @@ pipelineJob("${folderPath}/upgrade-start.jbpm.org") {
                     |
                     |Every configuration change needs to be done directly in the DSL files. See the below listed 'Seed job' for more info.
                     |
-                    |Pipeline job for bumping up and add kie-versions to start.jbpm.org.
+                    |Pipeline job for bumping up and add kie-versions or adding redhat product versions to start.jbpm.org.
                     |""".stripMargin())
 
     parameters {
         stringParam("CURRENT_KIE_VERSION", "${CURRENT_KIE_VERSION}", "the current kie-version in start.jbpm.org")
         stringParam("NEW_KIE_VERSION", "${NEW_KIE_VERSION}", "Final kie-version of latest community release")
+        choiceParam("IS_PROD",["NO", "YES"],"Please select yest if you want to update start.bpm.org by a new prod version")
+        stringParam("PROD_VERSION","${PROD_VERSION}","Edit the version for product (i.e. Enterprise 7.11.1: 7.52.0.Final-redhat-00008). Makes only sense if the previous parameter is YES)")
     }
 
     logRotator {
