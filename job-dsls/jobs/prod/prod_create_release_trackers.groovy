c5ecade96d2a549bba49510c21d88ae63aa802a4
Roberto Oliveira <rguimara@redhat.com>
2022-02-07 09:09:39 +0100

    [BXMSPROD-1608] add create_release_trackers job
    This job creates creates the release tracker tickets for each release

---
+++ job-dsls/jobs/prod/prod_create_release_trackers.groovy
@@ -0,0 +1,29 @@
+/**
+* Create the release tracker tickets
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_create_release_trackers.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = "PROD"
+folder(folderPath)
+
+pipelineJob("${folderPath}/create-release-trackers") {
+    description('This job creates the release trackers tickets required by each release.')
+
+    parameters {
+        stringParam('VERSION', '', ' The release candidate version, i.e. 7.12.1.CR1')
+        choiceParam('TEMPLATE', ['rhba', 'rhba_kogito'], 'The template file that describes the tickets that will be created')
+    }
+
+    logRotator {
+        numToKeep(32)
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+
+}
