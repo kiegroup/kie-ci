cc7807276625f62cb2be9fa86923c439efb4dab8
Roberto Oliveira <rguimara@redhat.com>
2022-03-15 04:47:09 -0300

    [BXMSPROD-1666] add create-images-templates-and-sources job (#1222)
    * [BXMSPROD-1666] add create-images-templates-and-sources job
    
    This job creates the OpenShift images templates and sources
    
    * [BXMSPROD-1666] include optional --cref parameter
    
    * refactor optional params and job parameters/description

---
+++ job-dsls/jobs/prod/prod_create_images_templates_and_sources.groovy
@@ -0,0 +1,31 @@
+/**
+* Create the OpenShift images templates and sources
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_create_images_templates_and_sources.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = "PROD"
+folder(folderPath)
+
+pipelineJob("${folderPath}/create-images-templates-and-sources") {
+    description('This job creates the Openshift images templates and sources, it must be run separately for RHPAM and RHDM.')
+
+    parameters {
+        stringParam('VERSION', '', ' The milestone version, i.e. 7.12.1')
+        stringParam('BUILDS', '', 'List of Brew builds separated by comma')
+        stringParam('OVERRIDING_FILE', 'branch-overrides.yaml', 'The overriding file that will be fetched from the images repositories')
+        stringParam('GITHUB_REFERENCE', '', 'Override the GitHub reference for all cloned repositories')
+    }
+
+    logRotator {
+        numToKeep(32)
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+
+}
