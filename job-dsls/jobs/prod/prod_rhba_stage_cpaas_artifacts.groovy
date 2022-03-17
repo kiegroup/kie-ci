280ca33156a9f3feea4ec7090f5014eee7d9bae7
Roberto Oliveira <rguimara@redhat.com>
2022-03-17 10:16:18 -0300

    [BXMSPROD-1667] add stage-cpaas-artifacts job

---
+++ job-dsls/jobs/prod/prod_rhba_stage_cpaas_artifacts.groovy
@@ -0,0 +1,32 @@
+/**
+* Stage artifacts produced by CPaaS into host rcm-guest.app.eng.bos.redhat.com.
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_rhba_stage_cpaas_artifacts.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = "PROD"
+folder(folderPath)
+
+pipelineJob("${folderPath}/stage-cpaas-artifacts") {
+    description("This job stages artifacts produced by CPaaS  into host rcm-guest.app.eng.bos.redhat.com. \n" +
+            "The staging is performed directly into the host through SSH and it copies artifacts from RHBA milestone folder to the proper product milestone staging folders.")
+
+    parameters {
+        stringParam('PRODUCT_NAME', 'RHPAM', 'Product name')
+        stringParam('VERSION', '', ' The release candidate version, i.e. 7.12.1.CR1')
+        stringParam("RCM_HOST", "rcm-guest.app.eng.bos.redhat.com", "rcm host")
+        stringParam("STAGING_BASE_PATH", "/mnt/rcm-guest/staging", "Staging base path inside the host")
+    }
+
+    logRotator {
+        numToKeep(32)
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+
+}
