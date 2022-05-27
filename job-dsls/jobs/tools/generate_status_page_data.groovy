fb640305d938d070797cb6bfa0dbdf6aa767a768
Enrique Mingorance <ginxo@hotmail.com>
2022-05-27 10:37:10 +0200

    Generate status page data job for RHBA/Kogito streams

---
+++ job-dsls/jobs/tools/generate_status_page_data.groovy
@@ -0,0 +1,34 @@
+/**
+* Create the OpenShift images templates and sources
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/generate_status_page_data.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = "Tools"
+folder(folderPath)
+
+pipelineJob("${folderPath}/rhba.generate-status-page-data") {
+    description('This job generates data for https://kiegroup.github.io/droolsjbpm-build-bootstrap')
+
+    logRotator {
+        numToKeep(20)
+    }
+
+    properties {
+        pipelineTriggers {
+            triggers {
+                cron{
+                    spec("H */2 * * *")
+                }
+            }
+        }
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+
+}
