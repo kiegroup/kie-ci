1eab6d417b54ebd584799c87512ad2a33c9012c6
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2022-05-30 12:16:00 +0200

    removed sending mails from tests (#1252)

--- job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/templates/additionalTests/JbpmContainerTestMatrix.groovy
+++ job-dsls/src/main/groovy/org/kie/jenkins/jobdsl/templates/additionalTests/JbpmContainerTestMatrix.groovy
@@ -81,7 +81,6 @@ class JbpmContainerTestMatrix {
 
             publishers {
                 archiveJunit("**/target/*-reports/TEST-*.xml")
-                mailer('kie-jenkins-builds@redhat.com', false, false)
                 wsCleanup()
             }
 
