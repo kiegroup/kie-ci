c2255bc9e5a5bea16ebeb5b246227c57a0ffbb7f
Roberto Oliveira <rguimara@redhat.com>
2022-08-05 07:48:23 +0200

    [BXMSPROD-1771] add osl-trigger-umb-message job

---
+++ job-dsls/jobs/prod/prod_osl_trigger_umb_message.groovy
@@ -0,0 +1,28 @@
+/**
+* Create the UMB message body for OpenShift Serverless Logic and invoke prod/send_umb_message job to send the message.
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_osl_trigger_umb_message.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = 'PROD'
+folder(folderPath)
+
+pipelineJob("${folderPath}/osl-trigger-umb-message") {
+    description('This job creates the OSL UMB message body and invoke prod/send_umb_message job to send the message.')
+
+    parameters {
+        stringParam('MILESTONE', '', 'The release milestone, i.e. 1.24.0.CR1. This parameter is optional and in case it is not defined, the latest available milestone will be used.')
+        stringParam("PNC_API_URL", "http://orch.psi.redhat.com/pnc-rest/v2", "PNC Rest API endpoint. See: https://docs.engineering.redhat.com/display/JP/User%27s+guide")
+    }
+
+    logRotator {
+        numToKeep(32)
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+}
