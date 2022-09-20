bbda6939e29e701b57c8ea105dcd80a622997e56
Andrea Lamparelli <a.lamparelli95@gmail.com>
2022-09-20 15:56:08 +0200

    [BXMSPROD-1805] Jenkins job for kogito sources generation (#1324)
    * Added jenkins script for kogito container sources generation

--- job-dsls/jobs/prod/prod_kogito_create_images_sources.groovy
+++ job-dsls/jobs/prod/prod_kogito_create_images_sources.groovy
@@ -1,17 +1,17 @@
 /**
-* Create the OpenShift images templates and sources
+* Create the OpenShift images sources
 */
-def scriptTemplate = this.getClass().getResource("job-scripts/prod_create_images_templates_and_sources.jenkinsfile").text
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_kogito_create_images_sources.jenkinsfile").text
 def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
 
 def folderPath = "PROD"
 folder(folderPath)
 
-pipelineJob("${folderPath}/create-images-templates-and-sources") {
-    description('This job creates the Openshift images templates and sources for RHPAM.')
+pipelineJob("${folderPath}/kogito-create-images-sources") {
+    description('This job creates the Openshift sources for Kogito.')
 
     parameters {
-        stringParam('VERSION', '', ' The milestone version, i.e. 7.12.1')
+        stringParam('VERSION', '', ' The milestone version, i.e. 1.13.1')
         stringParam('BUILDS', '', 'List of Brew builds separated by comma')
         stringParam('OVERRIDING_FILES', 'branch-overrides.yaml', 'The overriding files that will be fetched from the images repositories')
         stringParam('GITHUB_REFERENCE', '', 'Override the GitHub reference for all cloned repositories')
