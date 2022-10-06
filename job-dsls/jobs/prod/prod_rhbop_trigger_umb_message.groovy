a443c1f9761b44f50876b68c771b10ad517cf15e
Andrea Lamparelli <a.lamparelli95@gmail.com>
2022-10-06 18:15:51 +0200

    [RHBOP] Added UMB message generation for RHBOP prod release

---
+++ job-dsls/jobs/prod/prod_rhbop_trigger_umb_message.groovy
@@ -0,0 +1,31 @@
+/**
+* Create the UMB message body for OpenShift Serverless Logic and invoke prod/send_umb_message job to send the message.
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_rhbop_trigger_umb_message.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = 'PROD'
+folder(folderPath)
+
+pipelineJob("${folderPath}/rhbop-trigger-umb-message") {
+    description('This job creates the RHBOP UMB message body and invoke prod/send_umb_message job to send the message.')
+
+    parameters {
+        stringParam('PRODUCT_ID', '', 'The product ID. This parameter is optional and in case it is not defined, the default OSL product ID is used.')
+        stringParam('MILESTONE', '', 'The release milestone, i.e. 1.24.0.CR1. This parameter is optional and in case it is not defined, the latest available milestone is used.')
+        stringParam('QUARKUS_VERSION', '2.7.6.Final-redhat-00006', 'The productized version of Quarkus used for building Red Hat build of OptaPlanner currect milestone.')
+        stringParam("PNC_API_URL", "http://orch.psi.redhat.com/pnc-rest/v2", "PNC Rest API endpoint. See: https://docs.engineering.redhat.com/display/JP/User%27s+guide")
+        stringParam("INDY_URL", "http://indy.psi.redhat.com/browse/maven/hosted/pnc-builds/", "Indy PNC builds repository")
+    }
+
+    logRotator {
+        numToKeep(32)
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+}
