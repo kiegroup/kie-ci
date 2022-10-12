98b5d6cb29aa18ab24127e3eb1b8c9c9cc30bd8c
Andrea Lamparelli <a.lamparelli95@gmail.com>
2022-10-12 10:04:08 +0200

    [RHBOP] Added offliner in UMB msg

--- job-dsls/jobs/prod/prod_rhbop_trigger_umb_message.groovy
+++ job-dsls/jobs/prod/prod_rhbop_trigger_umb_message.groovy
@@ -12,7 +12,7 @@ pipelineJob("${folderPath}/rhbop-trigger-umb-message") {
 
     parameters {
         stringParam('PRODUCT_ID', '', 'The product ID. This parameter is optional and in case it is not defined, the default OSL product ID is used.')
-        stringParam('MILESTONE', '', 'The release milestone, i.e. 1.24.0.CR1. This parameter is optional and in case it is not defined, the latest available milestone is used.')
+        stringParam('MILESTONE', '', 'The release milestone, i.e. 8.29.0.CR1. This parameter is optional and in case it is not defined, the latest available milestone is used.')
         stringParam('QUARKUS_VERSION', '2.7.6.Final-redhat-00006', 'The productized version of Quarkus used for building Red Hat build of OptaPlanner currect milestone.')
         stringParam("PNC_API_URL", "http://orch.psi.redhat.com/pnc-rest/v2", "PNC Rest API endpoint. See: https://docs.engineering.redhat.com/display/JP/User%27s+guide")
         stringParam("INDY_URL", "http://indy.psi.redhat.com/browse/maven/hosted/pnc-builds/", "Indy PNC builds repository")
