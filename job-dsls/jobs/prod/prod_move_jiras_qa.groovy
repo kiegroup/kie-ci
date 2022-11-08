a0ddd218df49da478e656ad5b70ed7fbc29bd813
Roberto Oliveira <rguimara@redhat.com>
2022-11-08 14:28:47 +0100

    [BXMSPROD-1859] add support for RHBOP product on move-jiras-qa job (#1358)

--- job-dsls/jobs/prod/prod_move_jiras_qa.groovy
+++ job-dsls/jobs/prod/prod_move_jiras_qa.groovy
@@ -1,5 +1,5 @@
 /**
-* Move Jiras with status "Resolved" to "Ready for QA" for a specific target release
+* Move Jiras that were fixed on a specific target release to QA
 */
 def scriptTemplate = this.getClass().getResource("job-scripts/prod_move_jiras_qa.jenkinsfile").text
 def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
@@ -8,12 +8,12 @@ def folderPath = 'PROD'
 folder(folderPath)
 
 pipelineJob("${folderPath}/move-jiras-qa") {
-    description('This job moves Jiras wih status "Resolved" to "Ready for QA" for a specific target release.')
+    description('This job moves Jiras that were fixed on a specific target release to QA.')
 
     parameters {
-        stringParam('PRODUCT', 'rhpam,rhdm', 'Comma separated list of product, i.e rhpam,rhdm')
-        stringParam('VERSION', '7.13.2.CR1', 'The release candidate version, i.e. 7.13.2.CR1 or IBM BAMOE 8.0.2.CR1')
-        stringParam('PRODUCT_VERSION', '7.13.2.GA', 'Product target version, i.e 7.13.2.GA or IBM BAMOE 8.0.2.GA')
+        stringParam('PRODUCT', 'rhpam,rhdm', 'Comma separated list of product, i.e rhpam,rhdm / rhbop')
+        stringParam('VERSION', '7.13.2.CR1', 'The release candidate version, i.e. 7.13.2.CR1 / IBM BAMOE 8.0.2.CR1 / 8.29.0.CR1')
+        stringParam('PRODUCT_VERSION', '7.13.2.GA', 'Product target version, i.e 7.13.2.GA / IBM BAMOE 8.0.2.GA / 8.29.0.GA')
     }
 
     logRotator {
