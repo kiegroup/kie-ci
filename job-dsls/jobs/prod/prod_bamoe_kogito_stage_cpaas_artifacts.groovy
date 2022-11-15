52c9bd5f5316dc54a58461243a0b76303cfa5e41
Roberto Oliveira <rguimara@redhat.com>
2022-11-15 10:12:28 +0100

    [BXMSPROD-1883] add bamoe-kogito-stage-cpaas-artifacts job

---
+++ job-dsls/jobs/prod/prod_bamoe_kogito_stage_cpaas_artifacts.groovy
@@ -0,0 +1,33 @@
+/**
+* Stage BAMOE Kogito artifacts produced by CPaaS into host rcm-guest.app.eng.bos.redhat.com.
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_bamoe_kogito_stage_cpaas_artifacts.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = "PROD"
+folder(folderPath)
+
+pipelineJob("${folderPath}/bamoe-kogito-stage-cpaas-artifacts") {
+    description("This job stages BAMOE Kogito artifacts produced by CPaaS into host rcm-guest.app.eng.bos.redhat.com. \n" +
+            "The staging is performed directly into the host through SSH and it adjust artifacts for the given BAMOE Kogito milestone release.")
+
+    parameters {
+        stringParam('PRODUCT_NAME', 'bamoe-kogito', 'Product name')
+        stringParam('VERSION', '', ' The release candidate version, i.e. 1.13.2.CR1')
+        stringParam('BAMOE_VERSION', '', ' The BAMOE release candidate version related to the BAMOE Kogito release, i.e. 8.0.0.CR1')
+        stringParam('RCM_HOST', "\${RCM_HOST}", 'rcm host')
+        stringParam('STAGING_BASE_PATH', "\${RCM_GUEST_FOLDER}", 'Staging base path inside the host')
+    }
+
+    logRotator {
+        numToKeep(32)
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+
+}
