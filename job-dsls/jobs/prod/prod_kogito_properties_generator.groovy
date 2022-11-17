a7782f4fea0ec6f29e3334a87c76e5a2eb468f9d
Roberto Oliveira <rguimara@redhat.com>
2022-11-17 20:44:55 +0100

    [BXMSPROD-1885]  support Kogito productized releases in kogito-properties-generator job (#1370)

--- job-dsls/jobs/prod/prod_kogito_properties_generator.groovy
+++ job-dsls/jobs/prod/prod_kogito_properties_generator.groovy
@@ -17,20 +17,36 @@ node('${Constants.LABEL_KIE_RHEL}') {
             "REPO_URL"                          : REPO_URL_FINAL,
             "DELIVERABLE_REPO_URL"              : DELIVERABLE_REPO_URL,
             "KOGITO_PRODUCT_VERSION"            : KOGITO_PRODUCT_VERSION,
-            "KOGITO_PRODUCT_VERSION_LONG"       : KOGITO_PRODUCT_VERSION_LONG,
-            "OPTAPLANNER_PRODUCT_VERSION"       : OPTAPLANNER_PRODUCT_VERSION,
-            "OPTAPLANNER_PRODUCT_VERSION_LONG"  : OPTAPLANNER_PRODUCT_VERSION_LONG,
+            "RHPAM_PRODUCT_VERSION"             : RHPAM_PRODUCT_VERSION,
             "PRODUCT_MILESTONE"                 : PRODUCT_MILESTONE,
             "TIME_STAMP"                        : TIME_STAMP,
-            "GIT_INFORMATION_HASHES"            : GIT_INFORMATION_HASHES 
+            "KOGITO_VERSION"                    : KOGITO_VERSION,
+            "OPTAPLANNER_PRODUCT_VERSION"       : OPTAPLANNER_PRODUCT_VERSION,
+            "OPTAWEB_PRODUCT_VERSION"           : OPTAWEB_PRODUCT_VERSION,
+            "PLATFORM_QUARKUS_BOM_VERSION"      : PLATFORM_QUARKUS_BOM_VERSION,
+            "GIT_INFORMATION_HASHES"            : GIT_INFORMATION_HASHES
     ])
     if(Boolean.valueOf(IS_RELEASE)) {
-        println "//TODO"    
+        def folder = "kogito/kogito-\${KOGITO_PRODUCT_VERSION}.\${PRODUCT_MILESTONE}"
+
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${RELEASE_STAGING_FILE_ID}"],
+            [\$class: 'StringParameterValue', name: 'FILE_NAME', value: 'kogito-deliverable-list-staging.properties'],
+            [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${RELEASE_FILE_ID}"],
+            [\$class: 'StringParameterValue', name: 'FILE_NAME', value: 'kogito-deliverable-list.properties'],
+            [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
+
     } else {
         def folder = "kogito/KOGITO-\${KOGITO_PRODUCT_VERSION}.NIGHTLY"
 
         build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
-            [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${FILE_ID}"],
+            [\$class: 'StringParameterValue', name: 'NIGHTLY_FILE_ID', value: "\${NIGHTLY_FILE_ID}"],
             [\$class: 'StringParameterValue', name: 'FILE_NAME', value: "kogito-\${TIME_STAMP}.properties"],
             [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
             [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
@@ -49,15 +65,22 @@ pipelineJob("${folderPath}/kogito-properties-generator") {
     parameters {
         booleanParam("IS_RELEASE", true, "it defines if the properties file is for prod or not")
         stringParam("BRANCH_NAME", "main", "the branch the nightly was triggered for")
-        stringParam("REPO_URL", "http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/content/repositories/kogito-main-nightly", "Prod possibility is http://rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging")
-        stringParam("DELIVERABLE_REPO_URL", "http://download.devel.redhat.com/devel/candidates","")
-        stringParam("KOGITO_PRODUCT_VERSION", "1.0","")
-        stringParam("KOGITO_PRODUCT_VERSION_LONG", "1.0.redhat-00001", "This is just for prod files")
-        stringParam("OPTAPLANNER_PRODUCT_VERSION", "8.3.0","")
-        stringParam("OPTAPLANNER_PRODUCT_VERSION_LONG", "8.3.0.redhat-00001", "")
+        stringParam("REPO_URL", "http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/content/repositories/kogito-main-nightly", "Prod possibility is http://download.devel.redhat.com/rcm-guest/staging")
+        stringParam("DELIVERABLE_REPO_URL", "http://download.devel.redhat.com/devel/candidates")
+        stringParam("KOGITO_PRODUCT_VERSION", "1.13.0")
+        stringParam("RHPAM_PRODUCT_VERSION", "7.13.0")
         stringParam("PRODUCT_MILESTONE", "CR1", "This is just for prod files")
-        stringParam("TIME_STAMP", "", "This is just for prod files")
-        stringParam("FILE_ID", "kogito-nightly-properties-template", "The template file ID")
+        stringParam("TIME_STAMP", "", "This is just for non-prod files")
+        stringParam("GIT_INFORMATION_HASHES", "", "This is just for non-prod files")
+        stringParam("KOGITO_VERSION", "1.13.0.redhat-00001")
+        stringParam("OPTAPLANNER_PRODUCT_VERSION", "8.13.0.Final-redhat-00001")
+        stringParam("OPTAWEB_PRODUCT_VERSION", "8.13.0.Final-redhat-00001")
+        stringParam("PLATFORM_QUARKUS_BOM_VERSION", "2.7.6.Final-redhat-00006")
+        stringParam("RCM_GUEST_FOLDER", "\${RCM_GUEST_FOLDER}")
+        stringParam("RCM_HOST", "\${RCM_HOST}")
+        stringParam("NIGHTLY_FILE_ID", "kogito-nightly-properties-template")
+        stringParam("RELEASE_STAGING_FILE_ID", "kogito-prod-properties-template")
+        stringParam("RELEASE_FILE_ID", "kogito-prod-deliverable-properties-template")
     }
 
     definition {
