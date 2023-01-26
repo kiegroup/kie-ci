61a2eda2a23834c3acc9365e007069535fc4e4d6
Andrea Lamparelli <a.lamparelli95@gmail.com>
2023-01-26 10:25:17 +0100

    [BXMSPROD-1960] RHBOP: Adding quarkus platform version in UMB msg (#1421)

--- job-dsls/jobs/prod/prod_rhbop_trigger_umb_message.groovy
+++ job-dsls/jobs/prod/prod_rhbop_trigger_umb_message.groovy
@@ -13,7 +13,8 @@ pipelineJob("${folderPath}/rhbop-trigger-umb-message") {
     parameters {
         stringParam('PRODUCT_ID', '', 'The product ID. This parameter is optional and in case it is not defined, the default OSL product ID is used.')
         stringParam('MILESTONE', '', 'The release milestone, i.e. 8.29.0.CR1. This parameter is optional and in case it is not defined, the latest available milestone is used.')
-        stringParam('QUARKUS_VERSION', '2.7.6.Final-redhat-00006', 'The productized version of Quarkus used for building Red Hat build of OptaPlanner currect milestone.')
+        stringParam('QUARKUS_VERSION', '2.13.5.Final-redhat-00003', 'The productized version of Quarkus core used for building Red Hat build of OptaPlanner currect milestone.')
+        stringParam('QUARKUS_PLATFORM_VERSION', '2.13.5.SP1-redhat-00002', 'The productized version of Quarkus platform used for building Red Hat build of OptaPlanner currect milestone.')
         stringParam("PNC_API_URL", "\${ORCH_PSI_URL}/pnc-rest/v2", "PNC Rest API endpoint. See: \${DOCS_ENGINEERING_URL}/display/JP/User%27s+guide")
         stringParam("INDY_URL", "\${INDY_PNC_URL}", "Indy PNC builds repository")
     }
