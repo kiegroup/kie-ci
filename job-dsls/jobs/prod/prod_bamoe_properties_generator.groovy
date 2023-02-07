0ce9710f458b9bbfba3fc74fcc11a8aacee02231
Lucas Bacciotti <lbaciotti@gmail.com>
2023-02-07 14:41:31 +0000

    [BXMSPROD-1962] Replace mentions to RHPAM into BAMOE properties file for nightly and official releases (#1437)
    * [BXMSPROD-1962] - Setting up properties generator BAMOE file.
    
    * [BXMSPROD-1962] Customizing BAMOE prop. generator file.
    
    * Update job-dsls/jobs/prod/prod_bamoe_properties_generator.groovy
    
    Co-authored-by: Roberto Oliveira <robertoguimaraes8@gmail.com>
    
    ---------
    
    Co-authored-by: Roberto Oliveira <robertoguimaraes8@gmail.com>

--- job-dsls/jobs/prod/prod_bamoe_properties_generator.groovy
+++ job-dsls/jobs/prod/prod_bamoe_properties_generator.groovy
@@ -1,9 +1,12 @@
 /**
- * Generate properties files for productized builds.
+ * Generate properties files for nightly and productized builds.
+ *
+ * Generated files for nightly:
+ * - {bamoe}-{timestamp}.properties - properties file pointing to nightly binaries in the staging area
  *
  * Generated files for productized builds:
- * - {rhpam}-deliverable-list-staging.properties - properties file pointing binaries in the staging area
- * - {rhpam}-deliverable-list.properties - properties file pointing binaries in the candidates area
+ * - {bamoe-deliverable-list-staging.properties - properties file pointing binaries in the staging area
+ * - {bamoe}-deliverable-list.properties - properties file pointing binaries in the candidates area
  */
 import org.kie.jenkins.jobdsl.Constants
 
@@ -13,12 +16,19 @@ import groovy.json.JsonOutput
 node('${Constants.LABEL_KIE_RHEL}') {
 
     sh 'env\'
+    def REPO_URL_FOLDER_VERSION = 'main'.equals(BRANCH_NAME) ? 'main' : (PRODUCT_VERSION =~ /\\d+\\.\\d+/)[0]
+    println "Folder [\${REPO_URL_FOLDER_VERSION}] based on BRANCH_NAME [\${BRANCH_NAME}] and PRODUCT_VERSION [\${PRODUCT_VERSION}]"
+    def REPO_URL_FINAL = REPO_URL.replace("-main-", "-\${REPO_URL_FOLDER_VERSION}-")
+    println "REPO_URL_FINAL [\${REPO_URL_FINAL}]"
+
     def binding = JsonOutput.toJson([
-            "REPO_URL"                   : REPO_URL,
+            "REPO_URL"                   : REPO_URL_FINAL,
             "DELIVERABLE_REPO_URL"       : DELIVERABLE_REPO_URL,
+            "TAG_HASH"                   : TAG_HASH,
             "PRODUCT_VERSION"            : PRODUCT_VERSION,
             "PRODUCT_VERSION_LONG"       : PRODUCT_VERSION_LONG,
             "PRODUCT_MILESTONE"          : PRODUCT_MILESTONE,
+            "TIME_STAMP"                 : TIME_STAMP,
             "BOM_VERSION"                : BOM_VERSION,
             "KIE_VERSION"                : KIE_VERSION,
             "MVEL_VERSION"               : MVEL_VERSION,
@@ -26,20 +36,31 @@ node('${Constants.LABEL_KIE_RHEL}') {
             "INSTALLER_COMMONS_VERSION"  : INSTALLER_COMMONS_VERSION,
             "JAVAPARSER_VERSION"         : JAVAPARSER_VERSION
     ])
-    def folder = "bamoe/bamoe-\${PRODUCT_VERSION}.\${PRODUCT_MILESTONE}"
+    if(Boolean.valueOf(IS_RELEASE)) {
+        def folder = "bamoe/BAMOE-\${PRODUCT_VERSION}.\${PRODUCT_MILESTONE}"
 
-    build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
-        [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${RELEASE_STAGING_FILE_ID}"],
-        [\$class: 'StringParameterValue', name: 'FILE_NAME', value: 'rhpam-deliverable-list-staging.properties'],
-        [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
-        [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
-    ]
-    build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
-        [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${RELEASE_FILE_ID}"],
-        [\$class: 'StringParameterValue', name: 'FILE_NAME', value: 'rhpam-deliverable-list.properties'],
-        [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
-        [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
-    ]
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${RELEASE_STAGING_FILE_ID}"],
+            [\$class: 'StringParameterValue', name: 'FILE_NAME', value: 'bamoe-deliverable-list-staging.properties'],
+            [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${RELEASE_FILE_ID}"],
+            [\$class: 'StringParameterValue', name: 'FILE_NAME', value: 'bamoe-deliverable-list.properties'],
+            [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
+    } else {
+        def folder = "bamoe/BAMOE-\${PRODUCT_VERSION}.NIGHTLY"
+
+        build job: env.PROPERTIES_GENERATOR_PATH, parameters: [
+            [\$class: 'StringParameterValue', name: 'FILE_ID', value: "\${NIGHTLY_FILE_ID}"],
+            [\$class: 'StringParameterValue', name: 'FILE_NAME', value: "bamoe-\${TIME_STAMP}.properties"],
+            [\$class: 'StringParameterValue', name: 'FOLDER_PATH', value: folder],
+            [\$class: 'StringParameterValue', name: 'BINDING', value: binding]
+        ]
+    }
 }
 """
 // create needed folder(s) for where the jobs are created
@@ -47,22 +68,27 @@ def folderPath = "PROD"
 folder(folderPath)
 
 pipelineJob("${folderPath}/bamoe-properties-generator") {
-    description("Generate properties files for BAMOE productized builds")
+    description("Generate properties files for BAMOE nightly and productized builds")
 
     parameters {
-        stringParam("REPO_URL", "\${STAGING_SERVER_URL}")
+        booleanParam("IS_RELEASE", true, "it defines if the properties file is for prod or not")
+        stringParam("BRANCH_NAME", "main", "the branch the nightly was triggered for")
+        stringParam("TAG_HASH", "", "The hash of the nightly tag in productization repository. This is just for nightly")
+        stringParam("REPO_URL", "\${BXMS_QE_NEXUS}/content/repositories/rhba-main-nightly", "Prod possibility is \${STAGING_SERVER_URL}")
         stringParam("DELIVERABLE_REPO_URL", "\${DOWNLOAD_CANDIDATES}/middleware")
         stringParam("PRODUCT_VERSION", "8.0.0")
-        stringParam("PRODUCT_VERSION_LONG", "8.0.0.redhat-00001")
-        stringParam("PRODUCT_MILESTONE", "CR1")
-        stringParam("BOM_VERSION", "\${PRODUCT_VERSION_LONG}")
-        stringParam("KIE_VERSION", "7.67.2.Final-redhat-00001")
+        stringParam("PRODUCT_VERSION_LONG", "8.0.0.redhat-00001", "This is just for prod files")
+        stringParam("PRODUCT_MILESTONE", "CR1", "This is just for prod files")
+        stringParam("TIME_STAMP", "", "This is just for non-prod files")
+        stringParam("BOM_VERSION", "\${PRODUCT_VERSION_LONG}", "This is just for prod files")
+        stringParam("KIE_VERSION", "7.67.2.Final-redhat-00001", "This is just for prod files")
         stringParam("MVEL_VERSION", "2.4.12.Final-redhat-00001")
         stringParam("IZPACK_VERSION", "4.5.4.rhba-redhat-00017")
         stringParam("INSTALLER_COMMONS_VERSION", "2.4.0.rhba-redhat-00018")
-        stringParam("JAVAPARSER_VERSION", "3.13.10")
+        stringParam("JAVAPARSER_VERSION", "3.13.10", "This is just for prod files")
         stringParam("RCM_GUEST_FOLDER", "\${RCM_GUEST_FOLDER}")
         stringParam("RCM_HOST", "\${RCM_HOST}")
+        stringParam("NIGHTLY_FILE_ID", "bamoe-nightly-properties-template")
         stringParam("RELEASE_STAGING_FILE_ID", "bamoe-prod-properties-template")
         stringParam("RELEASE_FILE_ID", "bamoe-prod-deliverable-properties-template")
     }
