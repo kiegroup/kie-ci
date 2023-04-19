60ebfd8c113a9b469b0ca308dd72376e275467bd
Roberto Oliveira <rguimara@redhat.com>
2023-04-19 11:45:03 +0200

    [BXMSPROD-2017] add bamoe-create-images-templates-and-sources and bamoe-kogito-create-images-sources jobs

---
+++ job-dsls/jobs/prod/prod_bamoe_kogito_create_images_sources.groovy
@@ -0,0 +1,34 @@
+/**
+* Create the OpenShift images sources
+*/
+def scriptTemplate = this.getClass().getResource("job-scripts/prod_bamoe_kogito_create_images_sources.jenkinsfile").text
+def parsedScript = scriptTemplate.replaceAll(/<%=\s*(\w+)\s*%>/) { config[it[1]] ?: '' }
+
+def folderPath = "PROD"
+folder(folderPath)
+
+pipelineJob("${folderPath}/bamoe-kogito-create-images-sources") {
+    description('This job creates the Openshift sources for BAMOE Kogito.')
+
+    parameters {
+        stringParam('VERSION', '', ' The milestone version, i.e. 1.13.5')
+        stringParam("PRODUCT_MILESTONE", "CR1")
+        stringParam('BAMOE_VERSION', '', ' The BAMOE version related to the BAMOE Kogito release, i.e. 8.0.3')
+        stringParam('BUILDS', '', 'List of Brew builds separated by comma')
+        stringParam('OVERRIDING_FILES', 'bamoe-kogito-runtime-jvm-rhel8-overrides.yaml,bamoe-kogito-builder-rhel8-overrides.yaml', 'Comma separated list of the overriding files that will be fetched from the images repositories')
+        stringParam('GITHUB_REFERENCE', '', 'Override the GitHub reference for all cloned repositories')
+        booleanParam('UPLOAD_ARTIFACTS', true, 'If the generated artifacts should be uploaded to rcm-host')
+    }
+
+    logRotator {
+        numToKeep(20)
+    }
+
+    definition {
+        cps {
+            script(parsedScript)
+            sandbox()
+        }
+    }
+
+}
