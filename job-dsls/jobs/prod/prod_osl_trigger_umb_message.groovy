04f96243b3a5e3e4dffe44b882d581caee760a3d
Roberto Oliveira <rguimara@redhat.com>
2023-05-03 14:50:33 +0200

    [BXMSPROD-1984] add parameter for Drools prod version in osl-trigger-umb-message job

--- job-dsls/jobs/prod/prod_osl_trigger_umb_message.groovy
+++ job-dsls/jobs/prod/prod_osl_trigger_umb_message.groovy
@@ -13,6 +13,7 @@ pipelineJob("${folderPath}/osl-trigger-umb-message") {
     parameters {
         stringParam('PRODUCT_ID', '', 'The product ID. This parameter is optional and in case it is not defined, the default OSL product ID is used.')
         stringParam('MILESTONE', '', 'The release milestone, i.e. 1.29.0.CR1. This parameter is optional and in case it is not defined, the latest available milestone is used.')
+        stringParam('DROOLS_VERSION', '8.38.0.Final-redhat-00001', 'The productized version of Drools used for building Openshift Serverless Logic currect milestone.')
         stringParam('QUARKUS_PLATFORM_VERSION', '2.13.7.Final-redhat-00003', 'The productized version of Quarkus Platform used for building Openshift Serverless Logic currect milestone.')
         stringParam('QUARKUS_VERSION', '2.13.7.Final-redhat-00003', 'The productized version of Quarkus used for building Openshift Serverless Logic currect milestone.')
         stringParam('PNC_API_URL', "\${ORCH_PSI_URL}/pnc-rest/v2", "PNC Rest API endpoint. See: \${DOCS_ENGINEERING_URL}/display/JP/User%27s+guide")
